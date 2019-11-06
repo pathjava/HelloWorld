a3ada1c51b94058487fea14588ff7ea73e194beb
Oleg K <severniyman@gmail.com>
2019-11-06 15:31:37 +0300

    HelloWorld First addition1

--- src/ru/progwards/sever/HelloWorld.java
+++ src/ru/progwards/sever/HelloWorld.java
@@ -9,6 +9,10 @@ public class HelloWorld {
         return x + y;
     }
 
+    static int addition1(int x, int y){
+        return x + y;
+    }
+
     public static void main(String[] args) {
         println("Hello World!");
         println("World, are you hear me?");
