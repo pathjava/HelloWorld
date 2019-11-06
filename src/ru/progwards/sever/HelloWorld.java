6058e7e217a6864af0ee242c73daf7cb819b99a6
Oleg K <severniyman@gmail.com>
2019-11-06 15:23:42 +0300

    HelloWorld First

---
+++ src/ru/progwards/sever/HelloWorld.java
@@ -0,0 +1,18 @@
+package ru.progwards.sever;
+
+public class HelloWorld {
+    static void println(String str){
+        System.out.println(str);
+    }
+
+    static int addition(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        println("Hello World!");
+        println("World, are you hear me?");
+        System.out.print("Я знаю, что 7 + 5 = ");
+        System.out.println(addition(7, 5));
+    }
+}
