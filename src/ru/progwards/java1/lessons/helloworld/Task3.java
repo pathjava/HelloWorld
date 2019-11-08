1116ebf725b12cdfc04e360f1b1ae651b701bffb
Oleg K <severniyman@gmail.com>
2019-11-08 18:27:30 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/helloworld/Task3.java
+++ src/ru/progwards/java1/lessons/helloworld/Task3.java
@@ -3,36 +3,48 @@ package ru.progwards.java1.lessons.helloworld;
 public class Task3 {
     public static int addition(int x, int y){
         int z = x + y;
-        System.out.println("Вызвана функция addition() " +z);
+        System.out.print("Вызвана функция addition() ");
+        System.out.println(z);
         return z;
     }
 
     public static int subtraction(int x, int y){
         int w = x - y;
-        System.out.println("Вызвана функция subtraction() " +w);
+        System.out.print("Вызвана функция subtraction() ");
+        System.out.println(w);
         return w;
     }
 
     public static int multiplication(int x, int y){
         int d = x * y;
-        System.out.println("Вызвана функция multiplication() " +d);
+        System.out.print("Вызвана функция multiplication() ");
+        System.out.println(d);
         return d;
     }
 
     public static void main(String[] args) {
         int a = 34;
         int b = 55;
-        int x = a;
-        int y = b;
-        int c = x + y;
-        int z = addition(a, b);
-        int w = subtraction(a, b);
-        int d = multiplication(a, b);
+        int c;
 
-        System.out.println("a = "+ a);
-        System.out.println("b = "+ b);
-        System.out.println("a + b = "+ z);
-        System.out.println("a - b = "+ w);
-        System.out.println("a * b = "+ d);
+        System.out.print("a = ");
+        System.out.println(a);
+        System.out.print("b = ");
+        System.out.println(b);
+
+        System.out.println();
+
+        c = addition(a, b);
+        System.out.print("a + b = ");
+        System.out.println(c);
+
+        c = subtraction(a, b);
+        System.out.print("a - b = ");
+        System.out.println(c);
+
+        c = multiplication(a, b);
+        System.out.print("a * b = ");
+        System.out.println(c);
+
     }
 }
