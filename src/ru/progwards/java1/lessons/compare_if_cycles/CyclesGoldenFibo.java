766b257659095df59c5b99a8e282870e4725ba1e
Oleg K <severniyman@gmail.com>
2019-11-16 12:02:33 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,9 +1,15 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
-    // абсолютно нет никакого решения
+    // решение дал Григорий
     public static boolean containsDigit(int number, int digit){
-        return true;
+        if (number == 0 && digit == 0) return true;
+        int n = number;
+        while (n > 0) {
+            if (n % 10 == digit) return true;
+            n /= 10;
+        }
+        return false;
     }
 
     public static int fiboNumber(int n){
@@ -16,12 +22,18 @@ public class CyclesGoldenFibo {
         }
     }
 
-    // абсолютно нет никакого решения
+    // решение дал Григорий
     public static boolean isGoldenTriangle(int a, int b, int c){
-        return true;
+        float k = 0;
+        if (a == b) k = (float)a / c;
+        else if (b == c) k = (float)b / a;
+        else if (a == c) k = (float)a / b;
+        return k > 1.61703f && k < 1.61903f;
     }
 
     public static void main(String[] args) {
+        System.out.println(containsDigit(240, 24));
         System.out.println(fiboNumber(20));
+        System.out.println(isGoldenTriangle(25, 25, 25));
     }
 }
