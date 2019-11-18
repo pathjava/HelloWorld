98c5b79eb7d9ddf583ec6b359c64507524baf775
Oleg K <severniyman@gmail.com>
2019-11-18 11:27:14 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/sever/Test.java
+++ src/ru/progwards/sever/Test.java
@@ -1,26 +1,8 @@
 package ru.progwards.sever;
 
 class Test{
-    private String name;
-    private int age;
-    private String country;
+    public static void main(String[] args) {
 
-    public Test(){
-        country = "RU";
-    }
-    public Test(String name, int age){
-        this();
-        this.country = country;
-        this.name = name;
-        this.age = age;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getAge(){
-        return age;
-    }
-    public String getCountry(){
-        return country;
     }
 }
+
