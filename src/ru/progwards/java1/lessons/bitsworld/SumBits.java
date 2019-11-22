c74f8bc5d1dfbf8b752ac7a4eb1d748994e9304c
Oleg K <severniyman@gmail.com>
2019-11-22 20:22:26 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,17 +2,33 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static int sumBits(byte value) {
-        byte count;
-        for (count = 0; value != 0; count++) {
-            value &= (value - 1);
+        int result = 0;
+
+        while (value > 0) {
+            if ((value & 1) == 1) {
+                result++;
+            }
+            value = (byte) (value >> 1);
         }
-        return count;
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(sumBits((byte) 0b01101100));
     }
 
-    public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b00110101));
+
+//    public static int sumBits(byte value) {
+//        byte count;
+//        for (count = 0; value != 0; count++) {
+//            value &= (value - 1);
+//        }
+//        return count;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(sumBits((byte) 0b00110101));
 
 //        int res = 177;
 //        System.out.println("Number of one bits = " + Integer.bitCount(res));
-    }
+//    }
 }
