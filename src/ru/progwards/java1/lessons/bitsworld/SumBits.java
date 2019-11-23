a1aa0085fa10e356564e3c5ad9d3226e89b6bd96
Oleg K <severniyman@gmail.com>
2019-11-23 20:28:56 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -23,46 +23,42 @@ public class SumBits {
 //    }
 
     //данный метод показал Валерий на консультации 23.11.2019
-        int result = 0;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        value >>= 1;
-        if ((value & 1) == 1)
-            result++;
-        return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b00101110));
-    }
-
+//        int result = 0;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        value >>= 1;
+//        if ((value & 1) == 1)
+//            result++;
+//        return result;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(sumBits((byte) 0b00101110));
+//    }
 
     // данный метод подсмотрел в Гугл
-//    public static int sumBits(byte value) {
-//        byte count;
-//        for (count = 0; value != 0; count++) {
-//            value &= (value - 1);
-//        }
-//        return count;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(sumBits((byte) 0b00110101));
-//    }
+        byte count;
+        for (count = 0; value != 0; count++) {
+            value &= (value - 1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(sumBits((byte) 0b00110101));
+    }
 }
