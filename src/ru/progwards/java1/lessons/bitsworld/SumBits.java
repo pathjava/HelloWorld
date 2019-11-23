cd01c487168b8f35203425d1f770bb3d82e18ee8
Oleg K <severniyman@gmail.com>
2019-11-23 20:19:40 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -4,52 +4,52 @@ public class SumBits {
     public static int sumBits(byte value) {
     //данный метод долго мучал сам, но была ошибка при старшей 1 - на консультации разобрали причину
     // ошибка была  в нехватке  || value < -1 в условие цикла и определении result 0 или <0
-        int result;
-        if (value < 0)
-            result = 1;
-        else
-            result = 0;
-        while (value > 0 || value < -1) {
-            if ((value & 1) == 1) {
-                result++;
-            }
-            value = (byte) (value >> 1);
-        }
-        return result;
-    }
-    public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b10101110));
-    }
-
-    //данный метод показал Валерий на консультации 23.11.2019
-//        int result = 0;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
-//        value >>= 1;
-//        if ((value & 1) == 1)
-//            result++;
+//        int result;
+//        if (value < 0)
+//            result = 1;
+//        else
+//            result = 0;
+//        while (value > 0 || value < -1) {
+//            if ((value & 1) == 1) {
+//                result++;
+//            }
+//            value = (byte) (value >> 1);
+//        }
 //        return result;
 //    }
-//
 //    public static void main(String[] args) {
-//        System.out.println(sumBits((byte) 0b00101110));
+//        System.out.println(sumBits((byte) 0b10101110));
 //    }
+
+    //данный метод показал Валерий на консультации 23.11.2019
+        int result = 0;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        value >>= 1;
+        if ((value & 1) == 1)
+            result++;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumBits((byte) 0b00101110));
+    }
 
 
     // данный метод подсмотрел в Гугл
