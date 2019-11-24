a3f116cbeadef5b493b74a671cf3a94ee7ec6b89
Oleg K <severniyman@gmail.com>
2019-11-24 20:26:15 +0300

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/sever/Test2.java
+++ src/ru/progwards/sever/Test2.java
@@ -2,29 +2,46 @@ package ru.progwards.sever;
 
 
 public class Test2 {
-    static void decToBinary(int n)
-    {
-        // array to store binary number
-        int[] binaryNum = new int[32];
-
-        // counter for binary array
-        int i = 0;
-        while (n > 0) {
-            // storing remainder in binary array
-            binaryNum[i] = n % 2;
-            n = n / 2;
-            i++;
+//    static void decToBinary(int n)
+//    {
+//        // array to store binary number
+//        int[] binaryNum = new int[32];
+//
+//        // counter for binary array
+//        int i = 0;
+//        while (n > 0) {
+//            // storing remainder in binary array
+//            binaryNum[i] = n % 2;
+//            n = n / 2;
+//            i++;
+//        }
+//
+//        // printing binary array in reverse order
+//        for (int j = i - 1; j >= 0; j--)
+//            System.out.print(binaryNum[j]);
+//    }
+//
+//    // driver program
+//    public static void main(String[] args)
+//    {
+//        int n = 17;
+//        decToBinary(n);
+//    }
+public enum Grade{VERYBAD, BAD, SATISFACTORILY, GOOD, EXCELLENT, NOTDEFINED}
+    static Grade intToGrade(int grade) {
+        switch (grade) {
+            case 1:
+                return Grade.VERYBAD;
+            case 2:
+                return Grade.BAD;
+            case 3:
+                return Grade.SATISFACTORILY;
+            case 4:
+                return Grade.GOOD;
+            case 5:
+                return Grade.EXCELLENT;
+            default:
+                return Grade.NOTDEFINED;
         }
-
-        // printing binary array in reverse order
-        for (int j = i - 1; j >= 0; j--)
-            System.out.print(binaryNum[j]);
-    }
-
-    // driver program
-    public static void main(String[] args)
-    {
-        int n = 17;
-        decToBinary(n);
     }
 }
