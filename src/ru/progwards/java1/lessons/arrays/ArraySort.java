f2d0f0f96c3f087e5159a0985cb81d9f65488d08
Olegg <javapath@yandex.ru>
2019-12-04 16:02:04 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -3,22 +3,29 @@ package ru.progwards.java1.lessons.arrays;
 public class ArraySort {
     public static void sort(int[] a){
         //int i = a[0];
-        int j = a[0];
-        for (int i = 0; i < i+1; i++){
-            int temp = a[i];
-            a[i] = a[i+1];
-            a[i] = temp;
-            if (a[i] >= a.length){
-                for (j = i+1; j < j+1; j++){
-                    int temp2 = a[j];
+        //int j = a[0];
+//        for (int i = 0; i > i+1; i++){
+//            int temp = a[i];
+//            a[i] = a[i+1];
+//            a[i] = temp;
+//
+//        }
+        int n = a.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (a[j] > a[j+1])
+                {
+                    int temp = a[j];
                     a[j] = a[j+1];
-                    a[j] = temp2;
+                    a[j+1] = temp;
                 }
-            }
-        }
     }
 
     public static void main(String[] args) {
-        //System.out.println(a);
+//        ArraySort arraySort = new ArraySort();
+//        int[] a = {64, 34, 25, 12, 22, 11, 90};
+//        arraySort.sort(a);
+//        System.out.println(a);
+        //arraySort.printArray(a);
     }
 }
--- src/ru/progwards/sever/Test_H6_2.java
+++ src/ru/progwards/sever/Test_H6_2.java
@@ -1,5 +1,31 @@
 package ru.progwards.sever;
 
+import java.util.Arrays;
+
+//public class Test_H6_2 {
+//    public static void main(String[] args) {
+//        int [] mas = {11, 3, 14, 16, 7};
+//
+//        boolean isSorted = false;
+//        int buf;
+//        while(!isSorted) {
+//            isSorted = true;
+//            for (int i = 0; i < mas.length-1; i++) {
+//                if(mas[i] > mas[i+1]){
+//                    isSorted = false;
+//
+//                    buf = mas[i];
+//                    mas[i] = mas[i+1];
+//                    mas[i+1] = buf;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(mas));
+//    }
+//}
+
+
+
 public class Test_H6_2 {
     public static void main(String args[]) {
         int[] ar = {12,-3,8,1,19,22,0};
