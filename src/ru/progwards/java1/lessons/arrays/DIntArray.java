f888674d7d9176b3238eec055508c00862abea83
Olegg <javapath@yandex.ru>
2019-12-06 19:36:00 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -50,12 +50,9 @@ public class DIntArray {
     }
 
     public static void main(String[] args) {
-//        DIntArray dIntArray = new DIntArray();
-//
-//        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//        System.out.println(Arrays.toString(arr));
-//        dIntArray.add(15);
-//
-//        System.out.println(Arrays.toString(arr));
+        // как запустить отладку?
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        System.out.println(Arrays.toString(arr));
     }
 }
