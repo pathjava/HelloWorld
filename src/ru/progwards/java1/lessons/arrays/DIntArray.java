e3ab4c262028dfbaf6d56f98cac1fdf822935ee6
Olegg <javapath@yandex.ru>
2019-12-07 17:08:17 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -12,23 +12,33 @@ public class DIntArray {
 
     public void add(int num){
         int[] b = new int[a.length + 1];
+        //копируем массив от 0, в конце остается +1 ячейка
         System.arraycopy(a, 0, b, 0, a.length);
+        //помещаем num в конец массива (присваивваем ячейке)
         b[a.length] = num;
+        //присваиваем b в a
         a = b;
     }
 
     public void atInsert(int pos, int num){
         int[] b = new int[a.length + 1];
+        //копируем массив от 0 до значения позиции
         System.arraycopy(a, 0, b, 0, pos);
+        //копируем массив от значения позиции +1 до конца массива
         System.arraycopy(a, pos, b, pos + 1, a.length - pos);
+        //помещааем num в pos (присваивваем ячейке)
         b[pos] = num;
+        //присваиваем b в a
         a = b;
     }
 
     public void atDelete(int pos){
         int[] b = new int[a.length - 1];
+        //копируем массив от 0 до pos
         System.arraycopy(a, 0, b, 0, pos);
+        //копируем массив от pos +1 в индексе до конца, уменьшая длинну массива на -1
         System.arraycopy(a, pos + 1, b, pos, a.length - pos - 1);
+        //присваиваем b в a
         a = b;
     }
 
@@ -46,5 +56,6 @@ public class DIntArray {
         System.out.println(Arrays.toString(dIntArray.a));
         dIntArray.atDelete(7);
         System.out.println(Arrays.toString(dIntArray.a));
+        System.out.println("Значение в массиве под индексом " + dIntArray.at(2-1) + ": " + dIntArray.at(2));
     }
 }
