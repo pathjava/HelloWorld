04de91ad2dda2c0a4fb0c78d91f7c621791ffa3d
Olegg <javapath@yandex.ru>
2019-12-07 16:34:31 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,57 +3,48 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class DIntArray {
-    private int[] a;
+    //private int[] a = {};
+    // тестовый массив
+    private int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     DIntArray(){
     }
 
     public void add(int num){
-        /* public void add(int num) - добавляет элемент num в конец массива, при этом размер массива должен увеличиться на 1.
-        Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый, и добавить в хвост элемент num. */
-
-//        int len = a.length;
-//        int[] a = new int[len + 1];
-//        a = Arrays.copyOf(a, a.length + 1);
-//        a[a.length - 1] = num;
-
-        // ранее написанные варианты решеения
-        int[] a = new int[] {num};
-        a = Arrays.copyOf(a, a.length + 1);
-        a[a.length - 1] = num;
-
-//        int[] b = new int[a.length + 1];
-//        System.arraycopy(a, 0, b, 0, a.length);
-//        a = b;
+        int[] b = new int[a.length + 1];
+        System.arraycopy(a, 0, b, 0, a.length);
+        b[a.length] = num;
+        a = b;
     }
 
     public void atInsert(int pos, int num){
-        /* public void atInsert(int pos, int num) - добавляет элемент num в позицию pos массива, при этом размер массива должен увеличиться на 1.
-        Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый,
-        c учетом того, что новый элемент окажется где-то в середине, и потом положить в нужный индекс элемент num. */
-
-        int len = a.length;
-        int[] a = new int[len + 1];
-        a = Arrays.copyOf(a, a.length + 1);
-        a[a.length - 1] = num;
+        int[] b = new int[a.length + 1];
+        System.arraycopy(a, 0, b, 0, pos);
+        System.arraycopy(a, pos, b, pos + 1, a.length - pos);
+        b[pos] = num;
+        a = b;
     }
 
     public void atDelete(int pos){
-        /* public void atDelete(int pos) - удаляет элемент в позиции pos массива, при этом размер массива должен уменьшиться на 1.
-        Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый, уже без элемента, который был в позиции pos. */
-
-        // то же не понимаю как это реализовать, про удаление ячейки на лекции не помню информации. было про .set, но это перезапись значения
+        int[] b = new int[a.length - 1];
+        System.arraycopy(a, 0, b, 0, pos);
+        System.arraycopy(a, pos + 1, b, pos, a.length - pos - 1);
+        a = b;
     }
 
     public int at(int pos){
-        return pos;
+        return a[pos];
     }
 
     public static void main(String[] args) {
         DIntArray dIntArray = new DIntArray();
-        // как запустить отладку?
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        dIntArray.add(11);
-        System.out.println(Arrays.toString(arr));
+        // как сделать вызов массива для теста из main так и не понял
+        //int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        dIntArray.add(12);
+        System.out.println(Arrays.toString(dIntArray.a));
+        dIntArray.atInsert(5, 15);
+        System.out.println(Arrays.toString(dIntArray.a));
+        dIntArray.atDelete(7);
+        System.out.println(Arrays.toString(dIntArray.a));
     }
 }
