f6e37f1d011d651677903121bdf06b42b88206b5
Olegg <javapath@yandex.ru>
2019-12-17 16:04:34 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,42 +1,16 @@
 package ru.progwards.java1.lessons.interfaces;
 
 public class CalculateFibonacci {
-    // решение дал Григорий
-//    public static boolean containsDigit(int number, int digit){
-//        if (number == 0 && digit == 0) return true;
-//        int n = number;
-//        while (n > 0) {
-//            if (n % 10 == digit) return true;
-//            n /= 10;
-//        }
-//        return false;
-//    }
 
-    public static int fiboNumber(int n){
-        if (lastFibo.n == 0)
-            return lastFibo.fibo;
-            int f1 = 0;
-            int f2 = 1;
-            int t;
-            int i = 1;
-            while (i++ < n) {
-                t = f2;
-                f2 += f1;
-                f1 = t;
-            }
-            lastFibo.n = n;
-            lastFibo.fibo = f2;
-            return f2;
-    }
-    private static CacheInfo lastFibo;
-
+    static CacheInfo lastFibo;
     static {
         lastFibo = new CacheInfo();
     }
 
-    static class CacheInfo{
-        public int n;
-        public int fibo;
+    static class CacheInfo {
+
+        int n; //число, для которого рассчитываем Фибоначчи
+        int fibo; //результат расчета
 
         CacheInfo(){
             n = -1;
@@ -48,29 +22,24 @@ public class CalculateFibonacci {
         }
     }
 
-    static public CacheInfo getLastFibo(){
-        return lastFibo;
+    // Fibonacci n number
+    public static int fiboNumber(int n) {
+        if (lastFibo.n == n) return lastFibo.fibo;
+        int f1 = 0;
+        int f2 = 1;
+        int t;
+        int i = 1;
+        while (i++ < n) {
+            t = f2;
+            f2 += f1;
+            f1 = t;
+        }
+        lastFibo.n = n;
+        lastFibo.fibo = f2;
+        return f2;
     }
 
-    static public void clearLastFibo(){
-        lastFibo = null;
-    }
-
-    // решение дал Григорий
-//    public static boolean isGoldenTriangle(int a, int b, int c){
-//        float k = 0;
-//        if (a == b)
-//            k = (float)a / c;
-//        else if (b == c)
-//            k = (float)b / a;
-//        else if (a == c)
-//            k = (float)a / b;
-//        return k > 1.61703f && k < 1.61903f;
-//    }
-
-    public static void main(String[] args) {
-//        System.out.println(containsDigit(12345, 2));
-        System.out.println(fiboNumber(20));
-//        System.out.println(isGoldenTriangle(25, 25, 25));
+    CalculateFibonacci() {
+        lastFibo = new CacheInfo();
     }
 }
