e5fd386182b0ae8ff48674291f0c620d5eaf62db
Olegg <javapath@yandex.ru>
2019-12-18 12:37:49 +0530

    Revert "HelloWorld First addition1"
    This reverts commit a3ada1c5

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,6 +1,7 @@
 package ru.progwards.java1.lessons.interfaces;
 
 public class CalculateFibonacci {
+    // данное решение в значительной мере показал Григорий
     public static int fiboNumber(int n){
         if (lastFibo.n == 0)
             return lastFibo.fibo;
