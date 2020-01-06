68c5256eb12a9c2f585004ebd68175a14c5f27b9
Oleg K <severniyman@gmail.com>
2020-01-06 12:11:34 +0300

    H8

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,26 +1,25 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
+    public static void filterFile(String inFileName, String outFileName, String filter) {
         try{
             FileReader fileReader = new FileReader(inFileName);
             Scanner scanner = new Scanner(fileReader);
             FileWriter fileWriter = new FileWriter(outFileName);
             while (scanner.hasNextLine()) {
                 String str = scanner.nextLine();
-                str = str.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
-//                String strFilter = filter;
-//                int length = strFilter.length();
-//                for (int i = 1; i < length-1; i++){
-//                    char ch = strFilter.charAt(i);
-//                    str = str.replaceAll(String.valueOf(ch), "");
-//                }
+                // это рабочий вариант через регулярное выражение, но в нем нет гибкости применения
+//                str = str.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+                String strFilter = filter;
+                int length = strFilter.length();
+                for (int i = 0; i < length; i++){
+                    str = str.replace(String.valueOf(strFilter.charAt(i)), "");
+                }
                 fileWriter.write(str);
                 fileWriter.close();
             }
@@ -31,6 +30,6 @@ public class CharFilter {
 
     public static void main(String[] args) throws IOException {
         filterFile("src\\ru\\progwards\\java1\\lessons\\io1\\charFilterOne.txt",
-                "src\\ru\\progwards\\java1\\lessons\\io1\\charFilterTwo.txt", " -,.()");
+                "src\\ru\\progwards\\java1\\lessons\\io1\\charFilterTwo.txt", " -,.—()");
     }
 }
