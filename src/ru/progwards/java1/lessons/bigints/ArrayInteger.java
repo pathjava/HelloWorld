e99d41d9e2af9f408512e394783ff3b7bacf592f
Oleg K <severniyman@gmail.com>
2020-01-07 15:31:30 +0300

    H9

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -1,35 +1,83 @@
 package ru.progwards.java1.lessons.bigints;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class ArrayInteger {
-    byte[] digits;
-    int putVal;
+//    byte[] digits;
+//    int putVal;
+//
+//    ArrayInteger(int n){
+//        digits = new byte[n];
+//        putVal = 0;
+//    }
+//
+//    void fromInt(BigInteger value){
+//        if (value.compareTo(BigInteger.ZERO) == 0)
+//            return;
+//        while (value.compareTo(BigInteger.ZERO) > 0) {
+//            digits[putVal++] = value.mod(BigInteger.TEN).byteValueExact();
+//            value = value.divide(BigInteger.TEN);
+//        }
+//    }
+//
+//    BigInteger toInt(){
+//        BigInteger result = BigInteger.ZERO;
+//        for (Byte b : digits) {
+//            result = result.multiply(BigInteger.TEN);
+//            result = result.add(BigInteger.valueOf(b));
+//        }
+//        return result;
+//    }
+//
+//    boolean add(ArrayInteger num){
+//        return true;
+//    }
 
-    ArrayInteger(int n){
-        digits = new byte[n];
-        putVal = 0;
+    private final static BigInteger BI_TEN = BigInteger.valueOf(10);
+    private Byte[] digits;
+
+    public ArrayInteger(final int n){
+        digits = new Byte[n];
     }
 
-    void fromInt(BigInteger value){
-        if (value.compareTo(BigInteger.ZERO) == 0)
-            return;
-        while (value.compareTo(BigInteger.ZERO) > 0) {
-            digits[putVal++] = value.mod(BigInteger.TEN).byteValueExact();
-            value = value.divide(BigInteger.TEN);
+    public void fromInt(BigInteger value) {
+        final List<Byte> list = new ArrayList<>();
+        while (!value.equals(BigInteger.ZERO)) {
+            list.add(value.mod(BI_TEN).byteValueExact());
+            value = value.divide(BI_TEN);
         }
+        Collections.reverse(list);
+        digits = list.toArray(new Byte[list.size()]);
     }
 
-    BigInteger toInt(){
-        BigInteger result = BigInteger.ZERO;
+    public BigInteger toInt(){
+        BigInteger result = BigInteger.valueOf(0);
         for (Byte b : digits) {
-            result = result.multiply(BigInteger.TEN);
+            result = result.multiply(BI_TEN);
             result = result.add(BigInteger.valueOf(b));
         }
         return result;
     }
 
-    boolean add(ArrayInteger num){
+    public boolean add(ArrayInteger num){
+        int ten = 0;
+        final List<Byte> list = new ArrayList<>();
+        for (int i = Math.max(num.digits.length, this.digits.length)-1; i >=0 ; i--) {
+            int result = ((num.digits.length<=i) ? 0 : num.digits[i]) + ((this.digits.length<=i) ? 0 : this.digits[i]) + ten;
+            ten = result>9 ? 1 : 0;
+            list.add((byte)(result%10));
+        }
+        Collections.reverse(list);
+        digits = list.toArray(new Byte[list.size()]);
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return digits==null ? null : Arrays.toString(digits);
     }
 }
