c9dd3698c46bfc029a61856d876738bfb8de0c07
Oleg K <severniyman@gmail.com>
2020-01-07 15:11:11 +0300

    H9

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -3,33 +3,33 @@ package ru.progwards.java1.lessons.bigints;
 import java.math.BigInteger;
 
 public class ArrayInteger {
-//    byte[] digits;
-//    int putVal;
-//
-//    ArrayInteger(int n){
-//        digits = new byte[n];
-//        putVal = 0;
-//    }
-//
-//    void fromInt(BigInteger value){
-//        putVal = 0;
-//        if (value == 0) return;
-//        while (value > 0) {
-//            digits[putVal++] = value % 10
-//            value /= 10;
-//        }
-//
-//    }
-//
-//    BigInteger toInt(){
-//        byte[] rev = new byte[digits.length + 1];
-//        if (getVal == putVal){
-//            return BigInteger.ZERO;
-//        }
-//        return new BigInteger(rev);
-//    }
-//
-//    boolean add(ArrayInteger num){
-//        return true;
-//    }
+    byte[] digits;
+    int putVal;
+
+    ArrayInteger(int n){
+        digits = new byte[n];
+        putVal = 0;
+    }
+
+    void fromInt(BigInteger value){
+        if (value.compareTo(BigInteger.ZERO) == 0)
+            return;
+        while (value.compareTo(BigInteger.ZERO) > 0) {
+            digits[putVal++] = value.mod(BigInteger.TEN).byteValueExact();
+            value = value.divide(BigInteger.TEN);
+        }
+    }
+
+    BigInteger toInt(){
+        BigInteger result = BigInteger.ZERO;
+        for (Byte b : digits) {
+            result = result.multiply(BigInteger.TEN);
+            result = result.add(BigInteger.valueOf(b));
+        }
+        return result;
+    }
+
+    boolean add(ArrayInteger num){
+        return true;
+    }
 }
