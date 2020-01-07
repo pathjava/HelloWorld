6d5d98c3721920cc6d292065635841e757fda925
Oleg K <severniyman@gmail.com>
2020-01-07 17:59:11 +0300

    H9

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -25,7 +25,7 @@ public class Coder {
                     result += code[(int)symbol];
                 }
                 fileWriter.write(result);
-                scanner.close();
+//                scanner.close();
                 fileWriter.close();
             }
         } catch (IOException e){
