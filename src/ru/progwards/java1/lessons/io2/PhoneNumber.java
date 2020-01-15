c0e4b542fcaae6708d5cce3569452df707d54e24
Oleg K <severniyman@gmail.com>
2020-01-15 19:42:01 +0300

    H10

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -1,5 +1,13 @@
 package ru.progwards.java1.lessons.io2;
 
+/*
+1) заводим переменную formatNumber
+2) в цикле for строку phone переводим в массив toChartArray и условием if проверяем isDigit и помещаем в переменную formatNumber
+3) проверяем formatNumber на длину строки (как проверяем?) и если 11 цифр,
+методом substring(int from) забираем конечные 10 цифр и помещаем в formatNumber, после чего на первую позицию добавляе  +7
+4) далее методом insert(int offset, ...) поочередно вставляем ( и ), а также -
+5) делаем return formatNumber
+*/
 
 public class PhoneNumber {
     public static String format(String phone){
@@ -13,23 +21,23 @@ public class PhoneNumber {
         }
 
         if(formatNumber.length() < 10 || formatNumber.length() > 11) {
-            throw new RuntimeException("В номере телефон должно быть 10 или 11 цифр!");
+            throw new RuntimeException("В номере должно быть 10 или 11 цифр!");
         }
 
         if (formatNumber.length() >= 11){
+            // берем 10 цифр с конца строки
             formatNumber = (formatNumber.substring(formatNumber.length() - 10));
         }
-        StringBuilder stringBuilder1 = new StringBuilder(formatNumber);
-        stringBuilder1.insert(0, "+7");
-        stringBuilder1.insert(2, "(");
-        stringBuilder1.insert(6, ")");
-        stringBuilder1.insert(10, "-");
-        formatNumber = stringBuilder1.toString();
+        // вариант реализации через substring
+        return "+7(" + formatNumber.substring(0,3) + ")" + formatNumber.substring(3,6) + "-" + formatNumber.substring(6);
 
-        return  formatNumber;
+        // вариант реализации через StringBuilder и insert
+//        StringBuilder strFormat = new StringBuilder(formatNumber);
+//        strFormat.insert(0, "+7").insert(2, "(").insert(6, ")").insert(10, "-");
+//        return strFormat.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(format("911552-04-85"));
+        System.out.println(format("+(951) 132-04-75"));
     }
 }
--- src/ru/progwards/java1/lessons/io2/Translator.java
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -1,4 +1,61 @@
 package ru.progwards.java1.lessons.io2;
 
 public class Translator {
+    private String[] inLang;
+    private String[] outLang;
+
+    public Translator(String[] inLang, String[] outLang) {
+        this.inLang = inLang;
+        this.outLang = outLang;
+    }
+
+    public String translate(String sentence){
+        StringBuilder stringBuilders = new StringBuilder();
+//        sentence = sentence.toLowerCase();
+        String[] tempArr = sentence.split("(?<=\\b|[^\\p{L}])");
+        for (int i = 0; i < tempArr.length; i++) {
+            for (int j = 0; j < inLang.length; j++) {
+                char chTemp = tempArr[i].charAt(0);
+                if (tempArr[i].toLowerCase().equals(inLang[j])){
+                    tempArr[i] = outLang[j];
+                }
+                if(Character.isLowerCase(chTemp)){
+
+                } else if (Character.isUpperCase(chTemp)) {
+
+                }
+            } stringBuilders.append(tempArr[i]);
+        }
+        return stringBuilders.toString();
+    }
+
+    public static void main(String[] args) {
+        Translator test = new Translator(new String[]{"hello", "world", "java"}, new String[]{"привет", "мир", "джава"});
+        System.out.println(test.translate("Hello, World! How are you?"));
+    }
 }
+
+/*
+sentence = sentence.toLowerCase();
+        for (int i = 0; i < inLang.length; i++) {
+            if (sentence.contains(inLang[i])){
+                sentence = sentence.replace(inLang[i], outLang[i]);
+            }
+        }
+        return sentence;
+*/
+
+/*    public String translate(String sentence){
+        String temp = "";
+        sentence = sentence.toLowerCase();
+        String[] tempArr = sentence.split(" ");
+        for (int i = 0; i < tempArr.length; i++) {
+            // for each нужен для прохода по всему массиву каждый раз
+            for (int j = 0; j < inLang.length; j++) {
+                if (tempArr[i].equals(inLang[j])){
+                    tempArr[i] = outLang[j];
+                }
+            }
+        }
+        return Arrays.toString(tempArr);
+    }*/
