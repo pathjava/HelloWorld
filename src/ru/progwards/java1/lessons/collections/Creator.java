7b319c101d218400aadbf99a85665c6e9208257b
Oleg K <severniyman@gmail.com>
2020-01-20 12:29:40 +0300

    H11

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -2,7 +2,6 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,12 +13,12 @@ public class Creator {
 
         /* 2) данное решение сгенерировано средой Intellij IDEA из решения, представленного ниже
          * через цикл for и в данном решение уже нет привязки к типу листа - ArrayList или LinkedList */
-        List<Integer> fillList = IntStream.range(2, n).filter(i -> i % 2 == 0).boxed().collect(Collectors.toList());
+        Collection<Integer> fillList = IntStream.range(2, n).filter(i -> i % 2 == 0).boxed().collect(Collectors.toList());
         return fillList;
 
         /* 1) первоначальное решение через цикл for и ArrayList, но в условие задачи не сказано,
          * какой именно тип листа (ArrayList или LinkedList) */
-//        List<Integer> fillList = new ArrayList<>();
+//        Collection<Integer> fillList = new ArrayList<>();
 //        for (int i = 2; i < n; i++) {
 //            if (i % 2 == 0){
 //                fillList.add(i);
@@ -33,11 +32,11 @@ public class Creator {
 //        return IntStream.iterate(n, i -> i >= 1, i -> i - 1).filter(i -> i % 2 != 0).boxed().collect(Collectors.toList());
 
         /* так же как и у метода fillEven выше */
-        List<Integer> fillList = IntStream.iterate(n, i -> i >= 1, i -> i - 1).filter(i -> i % 2 != 0).boxed().collect(Collectors.toList());
+        Collection<Integer> fillList = IntStream.iterate(n, i -> i >= 1, i -> i - 1).filter(i -> i % 2 != 0).boxed().collect(Collectors.toList());
         return fillList;
 
         /* так же как и у метода fillEven выше */
-//        List<Integer> fillList = new ArrayList<>();
+//        Collection<Integer> fillList = new ArrayList<>();
 //        for (int i = n; i >= 1; i--) {
 //            if (i % 2 != 0) {
 //                fillList.add(i);
@@ -47,14 +46,21 @@ public class Creator {
     }
 
     public static Collection<Integer> fill3(int n){
-        List<Integer> fillList = new ArrayList<>();
-        for (int i = 2; i < (n * 3); i*=2) {
+//        Collection<Integer> fillList = new ArrayList<>();
+//        for (int i = 0; i < (n * 3); i++) {
+//            fillList.add(i);
+//            int sq = i * i;
+//            fillList.add(sq);
+//            fillList.add(sq * i);
+//        }
+//        return fillList;
+
+        /* более красивое решение с использованием статического метода Math.pow */
+        Collection<Integer> fillList = new ArrayList<>();
+        for (int i = 0; i < (n * 3); i++) {
             fillList.add(i);
-//            fillList.set(i-1, fillList.indexOf(i) * fillList.indexOf(i));
-            int index = fillList.indexOf(i);
-//            int index2 = (index * index * index);
-            int index2 = (int) Math.pow(index, 3);
-            fillList.set(index, index2);
+            fillList.add((int) Math.pow(i, 2));
+            fillList.add((int) Math.pow(i, 3));
         }
         return fillList;
     }
@@ -63,6 +69,6 @@ public class Creator {
     public static void main(String[] args) {
         System.out.println(fillEven(100));
         System.out.println(fillOdd(100));
-        System.out.println(fill3(100));
+        System.out.println(fill3(5));
     }
 }
