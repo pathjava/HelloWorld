3764669296426c0a4228083f6b57df92b27721f5
Oleg K <severniyman@gmail.com>
2020-01-23 18:06:52 +0300

    H11

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 public class ArrayIterator<T> implements Iterator<T> {
 
     private T[] array;
+    int index;
 
     ArrayIterator(T[] array) {
         this.array = array;
@@ -12,12 +13,18 @@ public class ArrayIterator<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
+        if(index < array.length){
+            return true;
+        }
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public T next() {
+        if(this.hasNext()){
+            return array[index++];
+        }
         // TODO Auto-generated method stub
         return null;
     }
