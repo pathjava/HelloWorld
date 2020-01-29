294bfb707c2271b55c8922a2d2fbfb33f6629d3c
Oleg K <severniyman@gmail.com>
2020-01-29 17:52:16 +0300

    Test 12

--- src/ru/progwards/sever/testprogwards/test_12/Test_01.java
+++ src/ru/progwards/sever/testprogwards/test_12/Test_01.java
@@ -7,24 +7,24 @@ import java.util.Set;
 
 public class Test_01 {
 
-//    public Set<Integer> a2set(int[] a){
-//        Set<Integer> arraySet = new HashSet<>();
-//        for(int arr : a){
-//            arraySet.add(arr);
-//        }
-//        return arraySet;
-//    }
+    public Set<Integer> a2set(int[] a){
+        Set<Integer> arraySet = new HashSet<>();
+        for(int arr : a){
+            arraySet.add(arr);
+        }
+        return arraySet;
+    }
 
-//    public static void iSetTest() {
-//        Set iSet = new HashSet<>();
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                iSet.add(i + j);
-//            }
-//        }
-//        System.out.println(iSet.size());
-//        System.out.println(iSet);
-//    }
+    public static void iSetTest() {
+        Set iSet = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                iSet.add(i + j);
+            }
+        }
+        System.out.println(iSet.size());
+        System.out.println(iSet);
+    }
 
     public static void wSetTest() {
         String TEXT = "на дворе трава на траве дрова не руби дрова на траве двора";
@@ -35,23 +35,23 @@ public class Test_01 {
             if (iter.next().contains("ра"))
                 iter.remove();
 
-        System.out.println(wordSet.size());
+//        System.out.println(wordSet.size());
     }
 
 
-//    public static void cset() {
-//        Set fiboSet1000 =
-//                Set.of(0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987);
-//        int sum = 0;
-//        for (int i = 2; i < 10; i++)
-//            sum += fiboSet1000.contains(i) ? 1 : 0;
-//        System.out.println(sum);
-//    }
+    public static void cset() {
+        Set fiboSet1000 =
+                Set.of(0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987);
+        int sum = 0;
+        for (int i = 2; i < 10; i++)
+            sum += fiboSet1000.contains(i) ? 1 : 0;
+        System.out.println(sum);
+    }
 
 
     public static void main(String[] args) {
-//        iSetTest();
-//        cset();
+        iSetTest();
+        cset();
         wSetTest();
     }
 }
