77c0879029e76793cab25de381813551df9401ce
Oleg K <severniyman@gmail.com>
2020-02-01 19:15:20 +0300

    H13

--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -1,6 +1,8 @@
 package ru.progwards.java1.lessons.queues;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderQueue {
     ArrayDeque<Order> thirdQueue = new ArrayDeque<>();
@@ -8,35 +10,70 @@ public class OrderQueue {
     ArrayDeque<Order> firstQueue = new ArrayDeque<>();
 
     public void add(Order order){
-        if (order.getSum() > 0 && order.getSum() <= 10000){
+        if (order.getSum() > 0 && order.getSum() <= 10000.0){
             thirdQueue.offer(order);
-        } else if (order.getSum() > 10000 && order.getSum() <= 20000){
+        } else if (order.getSum() > 10000.0 && order.getSum() <= 20000.0){
             secondQueue.offer(order);
         } else
             firstQueue.offer(order);
     }
 
+    Order returnOrder;
     public Order get(){
-        Order returnOrder = null;
         for (int i = 0; i < 3; i++) {
             switch (i){
                 case 0:
-                    while (!firstQueue.isEmpty()){
-                        returnOrder = firstQueue.poll();
+                    if (!firstQueue.isEmpty()){
+                        return firstQueue.poll();
                     }
                     break;
                 case 1:
-                    while (!secondQueue.isEmpty()){
-                        returnOrder = secondQueue.poll();
+                    if (!secondQueue.isEmpty()){
+                        return secondQueue.poll();
                     }
                     break;
                 case 2:
-                    while (!thirdQueue.isEmpty()){
-                        returnOrder = thirdQueue.poll();
+                    if (!thirdQueue.isEmpty()){
+                        return thirdQueue.poll();
                     }
                     break;
             }
         }
         return returnOrder;
+    }
+
+
+    public static void main(String[] args) {
+        OrderQueue orderQueue = new OrderQueue();
+        orderQueue.add(new Order(26257.0));
+        orderQueue.add(new Order(28723.0));
+        orderQueue.add(new Order(8258.0));
+        orderQueue.add(new Order(25805.0));
+        orderQueue.add(new Order(3005.0));
+        orderQueue.add(new Order(18765.0));
+        orderQueue.add(new Order(2705.0));
+        orderQueue.add(new Order(6447.0));
+        orderQueue.add(new Order(13275.0));
+        orderQueue.add(new Order(14840.0));
+        orderQueue.add(new Order(19946.0));
+        orderQueue.add(new Order(23877.0));
+        orderQueue.add(new Order(6494.0));
+        orderQueue.add(new Order(23694.0));
+        orderQueue.add(new Order(22783.0));
+        orderQueue.add(new Order(29282.0));
+        orderQueue.add(new Order(11130.0));
+        orderQueue.add(new Order(2443.0));
+        orderQueue.add(new Order(13402.0));
+        orderQueue.add(new Order(25664.0));
+        orderQueue.add(new Order(19901.0));
+        orderQueue.add(new Order(11239.0));
+        orderQueue.add(new Order(11138.0));
+        orderQueue.add(new Order(27393.0));
+
+        Order order = orderQueue.get();
+        while (order != null){
+            System.out.println(order);
+            order = orderQueue.get();
+        }
     }
 }
