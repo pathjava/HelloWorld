9e8f30788788a8a917530fd95fece39d92ec99ed
Oleg K <severniyman@gmail.com>
2020-02-01 20:24:28 +0300

    H13

--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -1,13 +1,18 @@
 package ru.progwards.java1.lessons.queues;
 
+import org.apache.logging.log4j.core.util.JsonUtils;
+
 import java.util.ArrayDeque;
 
 public class OrderQueue {
+    /* заводим три очереди для каждой группы заказов в зависимости от суммы заказа */
     ArrayDeque<Order> thirdQueue = new ArrayDeque<>();
     ArrayDeque<Order> secondQueue = new ArrayDeque<>();
     ArrayDeque<Order> firstQueue = new ArrayDeque<>();
 
+    /* метод, отвечающий за добавление заказов в очередь ArrayDeque */
     public void add(Order order){
+        /* проверяем сумму заказа и помещаем заказ в соответствующую очередь */
         if (order.getSum() > 0 && order.getSum() <= 10000.0){
             thirdQueue.offer(order);
         } else if (order.getSum() > 10000.0 && order.getSum() <= 20000.0){
@@ -16,20 +21,25 @@ public class OrderQueue {
             firstQueue.offer(order);
     }
 
+    /* метод, отвечающий за получение/вывод заказа */
     public Order get(){
+        /* если первая очередь не пустая, извлекаем первый заказ в очереди и возвращаем его в метод */
         if (!firstQueue.isEmpty()){
             return firstQueue.poll();
+        /* если первая очередь дошла до конца и пустая, переходим ко второй очереди и т.д. */
         } else if (!secondQueue.isEmpty()){
             return secondQueue.poll();
         } else if (!thirdQueue.isEmpty()){
             return thirdQueue.poll();
         }
+        /* когда все очереди заказов пустые, возвращаем null */
         return null;
     }
 
 
     public static void main(String[] args) {
         OrderQueue orderQueue = new OrderQueue();
+        /* передаем в метод add(Order order) параметр (сумму заказа) */
         orderQueue.add(new Order(26257.0));
         orderQueue.add(new Order(28723.0));
         orderQueue.add(new Order(8258.0));
