a2cd2be2c7dd29cbf6743910aac942e29b52eff9
Oleg K <severniyman@gmail.com>
2020-02-02 15:29:30 +0300

    H13

--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -6,10 +6,16 @@ public class StackCalc {
     ArrayDeque<Double> arrayDeque = new ArrayDeque<>();
 
     public void push(double value){
-        arrayDeque.push(value);
+        if (!(value == 0)) {
+            arrayDeque.push(value);
+        }
+//        System.out.println(arrayDeque);
     }
 
     public double pop(){
+        if (arrayDeque.isEmpty()){
+            System.out.println("array is null");
+        }
         return arrayDeque.pop();
     }
 
@@ -25,9 +31,45 @@ public class StackCalc {
         arrayDeque.push(one + two);
     }
 
+    public void sub(){
+        double one = 0;
+        double two = 0;
+        if (!arrayDeque.isEmpty()) {
+            one = arrayDeque.poll();
+        }
+        if (!arrayDeque.isEmpty()) {
+            two = arrayDeque.poll();
+        }
+        arrayDeque.push(two - one);
+    }
+
+    public void mul(){
+        double one = 0;
+        double two = 0;
+        if (!arrayDeque.isEmpty()) {
+            one = arrayDeque.poll();
+        }
+        if (!arrayDeque.isEmpty()) {
+            two = arrayDeque.poll();
+        }
+        arrayDeque.push(one * two);
+    }
+
+    public void div(){
+        double one = 0;
+        double two = 0;
+        if (!arrayDeque.isEmpty()) {
+            one = arrayDeque.poll();
+        }
+        if (!arrayDeque.isEmpty()) {
+            two = arrayDeque.poll();
+        }
+        arrayDeque.push(one / two);
+    }
+
 
     public static void main(String[] args) {
-        ArrayDeque<Double> arrDeq = new ArrayDeque<>(List.of(10.2,45.11,14.124,208.1,5.783));
+        ArrayDeque<Double> arrDeq = new ArrayDeque<>(List.of(10.2,45.11,14.124,28.1,5.783,15.02,20.154));
 //        final int ELEMENT = 10;
 //        Random random = new Random();
 //
@@ -42,6 +84,14 @@ public class StackCalc {
 
         System.out.println(calc.arrayDeque);
         System.out.println(calc.pop());
+        System.out.println(calc.arrayDeque);
+        calc.add();
+        System.out.println(calc.arrayDeque);
+        calc.sub();
+        System.out.println(calc.arrayDeque);
+        calc.mul();
+        System.out.println(calc.arrayDeque);
+        calc.div();
         System.out.println(calc.arrayDeque);
     }
 }
