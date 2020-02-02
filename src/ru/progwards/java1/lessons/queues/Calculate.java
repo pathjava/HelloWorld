121102f927fa610c1997504a3198f8166c970e94
Oleg K <severniyman@gmail.com>
2020-02-02 15:31:18 +0300

    H13

--- src/ru/progwards/java1/lessons/queues/Calculate.java
+++ src/ru/progwards/java1/lessons/queues/Calculate.java
@@ -1,11 +1,11 @@
 package ru.progwards.java1.lessons.queues;
 
 public class Calculate {
-//    public static double calculation1(){
-//
-//    }
-//
-//    public static double calculation2(){
-//
-//    }
+    public static double calculation1(){
+        return 0;
+    }
+
+    public static double calculation2(){
+        return 0;
+    }
 }
