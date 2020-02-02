51ce2e93b80d2e43b34eea43418f4ac25765fc3a
Oleg K <severniyman@gmail.com>
2020-02-02 20:30:33 +0300

    H13

--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -3,31 +3,36 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class StackCalc {
+    /* создаем массив для стека */
     ArrayDeque<Double> arrayDeque = new ArrayDeque<>();
 
+    /* заполняем массив при условии, что value не равно нулю */
     public void push(double value){
         if (!(value == 0)) {
             arrayDeque.push(value);
         }
-//        System.out.println(arrayDeque);
     }
 
+    /* если стек не пустой получаем и удаляем верхнее значение в стеке */
     public double pop(){
         if (arrayDeque.isEmpty()){
-            System.out.println("array is null");
+            System.out.println("arrayDeque is null");
         }
         return arrayDeque.pop();
     }
 
+    /* заводим две переменные, проверяем не пустой ли стек и извлекаем из него
+    * первые значения по очереди, помещая их в переменные */
     public void add(){
         double one = 0;
         double two = 0;
         if (!arrayDeque.isEmpty()) {
-            one = arrayDeque.poll();
+            one = arrayDeque.pop();
         }
         if (!arrayDeque.isEmpty()) {
-            two = arrayDeque.poll();
+            two = arrayDeque.pop();
         }
+        /* складываем значения двух переменных и результат помещаем на вершину стека */
         arrayDeque.push(one + two);
     }
 
@@ -35,11 +40,12 @@ public class StackCalc {
         double one = 0;
         double two = 0;
         if (!arrayDeque.isEmpty()) {
-            one = arrayDeque.poll();
+            one = arrayDeque.pop();
         }
         if (!arrayDeque.isEmpty()) {
-            two = arrayDeque.poll();
+            two = arrayDeque.pop();
         }
+        /* вычитаем из одной переменной другую и результат помещаем на вершину стека */
         arrayDeque.push(two - one);
     }
 
@@ -47,11 +53,12 @@ public class StackCalc {
         double one = 0;
         double two = 0;
         if (!arrayDeque.isEmpty()) {
-            one = arrayDeque.poll();
+            one = arrayDeque.pop();
         }
         if (!arrayDeque.isEmpty()) {
-            two = arrayDeque.poll();
+            two = arrayDeque.pop();
         }
+        /* умножаем значения двух переменных и результат помещаем на вершину стека */
         arrayDeque.push(one * two);
     }
 
@@ -59,29 +66,28 @@ public class StackCalc {
         double one = 0;
         double two = 0;
         if (!arrayDeque.isEmpty()) {
-            one = arrayDeque.poll();
+            one = arrayDeque.pop();
         }
         if (!arrayDeque.isEmpty()) {
-            two = arrayDeque.poll();
+            two = arrayDeque.pop();
         }
+        /* делим значение одной переменной на другую и результат помещаем на вершину стека */
         arrayDeque.push(two / one);
     }
 
 
     public static void main(String[] args) {
+        /* создаем массив и заполняем его значениями */
         ArrayDeque<Double> arrDeq = new ArrayDeque<>(List.of(10.2,45.11,14.124,28.1,5.783,15.02,20.154));
-//        final int ELEMENT = 10;
-//        Random random = new Random();
-//
-//        for (int i = 0; i < ELEMENT; i++) {
-//            arrDeq.push(random.nextDouble());
-//        }
 
+        /* создаем объект класса */
         StackCalc calc = new StackCalc();
+        /* в цикле передаем значения из массива в стек (он же массив) arrayDeque */
         for (Double aDouble : arrDeq) {
             calc.push(aDouble);
         }
 
+        /* вызываем методы и выводим результаты на консоль */
         System.out.println(calc.arrayDeque);
         System.out.println(calc.pop());
         System.out.println(calc.arrayDeque);
