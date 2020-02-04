059bd3cf36f929250cd909124f8f39abb16635bc
Oleg K <severniyman@gmail.com>
2020-02-04 19:52:49 +0300

    H14

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -1,21 +1,47 @@
 package ru.progwards.java1.lessons.maps;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class FiboMapCache {
-    private Map<Integer, BigDecimal> fiboCache;
+    private Map<Integer, BigDecimal> fiboCache = null;
+    private boolean cacheOn = false;
 
     public FiboMapCache(boolean cacheOn){
-
+        this.cacheOn = cacheOn;
     }
 
     public BigDecimal fiboNumber(int n){
-        return BigDecimal.ZERO;
+        if (cacheOn && fiboCache != null){
+            BigDecimal cacheResult = fiboCache.get(n);
+            if (cacheResult != null){
+                return cacheResult;
+            }
+        }
+        //проверить есть ли -1 и -2
+        int n0 = 1;
+        int n1 = 1;
+        int n2 = 0;
+        for(int i = 3; i <= n; i++){
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
+            // ложить рассчитанные фибоначи в мап
+        }
+        if (cacheOn) {
+            if (fiboCache == null) {
+                fiboCache = new TreeMap<>();
+            }
+            fiboCache.put(n, BigDecimal.valueOf(n2));
+        }
+
+        return BigDecimal.valueOf(n2);
     }
 
     public void clearCahe(){
-
+        fiboCache = null;
     }
 
     public static void test(){
--- src/ru/progwards/java1/lessons/maps/UsageFrequency.java
+++ src/ru/progwards/java1/lessons/maps/UsageFrequency.java
@@ -1,14 +1,12 @@
 package ru.progwards.java1.lessons.maps;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class UsageFrequency {
-
     ArrayList<Character > charsList = new ArrayList<>();
     ArrayList<String> wordsList = new ArrayList<>();
 
@@ -22,11 +20,6 @@ public class UsageFrequency {
                         char temp = words[i].charAt(0);
                         if (Character.isLetter(temp)){
                             wordsList.add(words[i]);
-                        }
-                    }
-                    for (int i = 0; i < words.length; i++) {
-                        char temp = words[i].charAt(0);
-                        if (Character.isAlphabetic(temp)) {
                             for (int j = 0; j < words[i].length(); j++) {
                                 char ch = words[i].charAt(j);
                                 if (Character.isLetter(ch)) {
@@ -38,16 +31,37 @@ public class UsageFrequency {
                 }
             }
             System.out.println(wordsList);
-            System.out.println(wordsList.size());
+//            System.out.println(wordsList.size());
             System.out.println();
             System.out.println(charsList);
-            System.out.println(charsList.size());
+            System.out.println();
+//            System.out.println(charsList.size());
         } catch(Throwable e){
             throw new RuntimeException(e);
         }
     }
 
+    public Map<Character, Integer> getLetters(){
+        TreeMap<Character, Integer> countChars = new TreeMap<>();
+        int count = 1;
+        for (int i = 0; i < charsList.size(); i++) {
+            Integer oldVal = countChars.putIfAbsent(charsList.get(i), count);
+            if (oldVal == null) count++;
+        }
+        System.out.println(countChars);
+        return countChars;
+    }
 
+    public Map<String, Integer> getWords(){
+        TreeMap<String, Integer> countWords = new TreeMap<>();
+        int count = 1;
+        for (int i = 0; i < wordsList.size(); i++) {
+            Integer oldVal = countWords.putIfAbsent(String.valueOf(wordsList.get(i)), count);
+            if (oldVal == null) count++;
+        }
+        System.out.println(countWords);
+        return countWords;
+    }
 
 
 
@@ -55,5 +69,7 @@ public class UsageFrequency {
         UsageFrequency test = new UsageFrequency();
         test.processFile("src/ru/progwards/java1/lessons/maps/wiki.test.tokens");
 //        System.out.println(test);
+        test.getLetters();
+        test.getWords();
     }
 }
