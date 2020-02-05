3162ff3cf0ef31bde127dc2825397f42d98c7d23
Oleg K <severniyman@gmail.com>
2020-02-05 18:50:28 +0300

    H14

--- src/ru/progwards/java1/lessons/maps/UsageFrequency.java
+++ src/ru/progwards/java1/lessons/maps/UsageFrequency.java
@@ -11,33 +11,45 @@ public class UsageFrequency {
         try (FileReader fileReader = new FileReader(fileName); Scanner scanner = new Scanner(fileReader)) {
             StringBuilder tempString;
             while (scanner.hasNext()){
+                /* обнуляем переменную при каждом новом цикле */
                 tempString = new StringBuilder();
                 String str = scanner.nextLine();
+                /* проверяем, не пустая ли полученная строка и если нет, запускаем цикл */
                 if (!str.isEmpty()) {
                     for (int i = 0; i < str.length(); i++) {
                         char temp = str.charAt(i);
+                        /* проверяем, если символ не является буквой или цифрой (это может быть любой знак препинания и т.д.),
+                        * тогда заменяем его пробелом - это на тот случай, когда два слова могут быть связаны символом (что-то, 12(а) и т.д.) */
                         if (!(Character.isLetterOrDigit(temp))){
                             tempString.append(" ");
+                            /* если условие выше не истина, тогда полученный символ буква или цифра и добавляем её */
                         } else
                             tempString.append(temp);
                     }
-
+                    /* сконкатенированную строку StringBuilder tempString конвертируем в массив с делением по пробелу */
                     String[] strTemp = tempString.toString().split(" ");
-                    for (int i = 0; i < strTemp.length; i++) {
-                        if (!strTemp[i].isEmpty()) {
-                            char ch = strTemp[i].charAt(0);
-                            if (Character.isDigit(ch) && strTemp[i].length() == 1){
+                    for (String s : strTemp) {
+                        /* проверяем, не пустое ли значение получено,
+                        * если не проверить, то на строке char ch = s.charAt(0); возможно падение программы */
+                        if (!s.isEmpty()) {
+                            char ch = s.charAt(0);
+                            /* отбираем одиночные цифры и отправляем в лист
+                            * это необходимо сделать, так как робот, проверяющий задачу считаем цисла из двух и более цифр как слова */
+                            if (Character.isDigit(ch) && s.length() == 1) {
                                 charsList.add(ch);
+                                /* отбираем слова и отправляем в лист
+                                * вэтом условие очищаем данные от пробелов, попавших в массив из StringBuilder tempString */
                             } else if (Character.isLetterOrDigit(ch)) {
-                                wordsList.add(strTemp[i]);
+                                wordsList.add(s);
                             }
                         }
                     }
                 }
             }
-            for (int i = 0; i < wordsList.size(); i++) {
-                for (int j = 0; j < wordsList.get(i).length(); j++) {
-                    char ch = wordsList.get(i).charAt(j);
+            /* в цикле берем слова из листа и разбираем их на отдельные символы, помещая в лист символов */
+            for (String s : wordsList) {
+                for (int j = 0; j < s.length(); j++) {
+                    char ch = s.charAt(j);
                     charsList.add(ch);
                 }
             }
@@ -48,13 +60,16 @@ public class UsageFrequency {
 
     public Map<Character, Integer> getLetters(){
         TreeMap<Character, Integer> countChars = new TreeMap<>();
-        int count = 1;
-        for (int i = 0; i < charsList.size(); i++) {
-            if (countChars.containsKey(charsList.get(i))){
-                Integer value = countChars.get(charsList.get(i));
-                countChars.put(charsList.get(i), (value + 1));
+        final int COUNT = 1;
+        /* цикле проверяем, если ли мапе countChars ключ, полученные из charsList
+        * и если Да, то получаем значение по ключу и при добавление пару ключ/значение увеличиваем значеение на +1 */
+        for (Character character : charsList) {
+            if (countChars.containsKey(character)) {
+                Integer value = countChars.get(character);
+                countChars.put(character, (value + 1));
             } else
-                countChars.put(charsList.get(i), count);
+                /* если ключ добавляется первый раз, то в качестве значения ему присваивается 1 */
+                countChars.put(character, COUNT);
         }
 
         return countChars;
@@ -62,13 +77,13 @@ public class UsageFrequency {
 
     public Map<String, Integer> getWords(){
         TreeMap<String, Integer> countWords = new TreeMap<>();
-        int count = 1;
-        for (int i = 0; i < wordsList.size(); i++) {
-            if (countWords.containsKey(wordsList.get(i))){
-                Integer value = countWords.get(wordsList.get(i));
-                countWords.put(wordsList.get(i), (value + 1));
+        final int COUNT = 1;
+        for (String s : wordsList) {
+            if (countWords.containsKey(s)) {
+                Integer value = countWords.get(s);
+                countWords.put(s, (value + 1));
             } else
-                countWords.put(wordsList.get(i), count);
+                countWords.put(s, COUNT);
         }
         return countWords;
     }
