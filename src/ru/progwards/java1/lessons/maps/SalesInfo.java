4bc74d9fd0a59d17454e603ca6c54299831498e6
Oleg K <severniyman@gmail.com>
2020-02-07 20:11:48 +0300

    H14

--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -3,32 +3,38 @@ package ru.progwards.java1.lessons.maps;
 import java.io.FileReader;
 import java.util.*;
 
-import static java.util.AbstractMap.*;
-
 public class SalesInfo {
     private TreeMap<Integer, ArrayList<String>> treeMap = new TreeMap<>();
+    /* переменная, используемая в определение проверяемой строки в методе isOnlyLettersOrDigits */
     private static int count;
     public int loadOrders(String fileName){
+        /* переменная учета успешно добавленных строк с данными в treeMap */
         int indexMap = 1;
         try (FileReader fileReader = new FileReader(fileName); Scanner scanner = new Scanner(fileReader)) {
             ArrayList<String> list;
+            /* "якорь" к которому происходит возврат из цикла при false.
+            как это называется не знаю - нашел по запросу как выйти во внешний цикл */
             outerloop:
             while (scanner.hasNext()){
                 count = 0;
                 list = new ArrayList<>();
                 String str = scanner.nextLine();
                 String[] arr = str.split("[\\\\;,]");
+                /* проверяем, соответствует ли массив заданной длине */
                 if ((!(arr.length == 4))){
                     continue;
                 }
+                /* берем элементы массива и в функции isOnlyLettersOrDigits проверяем, соответствуют ли они заданным параметрам.
+                * если нет, выходим из цикла во внешний while. если соответствует, то добавляем в list */
                 for (String s : arr) {
-                    if (!(isOnlyLetters(s))) {
+                    if (!(isOnlyLettersOrDigits(s))) {
                         continue outerloop;
                     } else {
                         count++;
                         list.add(s.trim());
                     }
                 }
+                /* добавляем в TreeMap в качестве ключа цифру, в качестве значения массив ArrayList */
                 treeMap.put(indexMap, list);
                 indexMap++;
             }
@@ -38,7 +44,8 @@ public class SalesInfo {
         return indexMap - 1;
     }
 
-    private boolean isOnlyLetters(String array){
+    /* метод проверки соответствия входных данных */
+    private boolean isOnlyLettersOrDigits(String array){
         char ch = ' ';
         boolean safe;
         int failCount = 0;
@@ -76,6 +83,7 @@ public class SalesInfo {
         TreeMap<String, Double> goodsList = new TreeMap<>();
 
         for (Map.Entry<Integer, ArrayList<String>> entry : treeMap.entrySet()) {
+            /* если значение get(1) из ArrayList уже есть в goodsList, тогда из goodsList получаем значение и суммируем с добавляемым */
             if (goodsList.containsKey(entry.getValue().get(1))){
                 Double sum = goodsList.get(entry.getValue().get(1));
                 goodsList.put(entry.getValue().get(1), (Double.parseDouble(entry.getValue().get(3))+ sum));
@@ -89,13 +97,11 @@ public class SalesInfo {
         TreeMap<String, AbstractMap.SimpleEntry<Double, Integer>> goodsList = new TreeMap<>();
 
         for (Map.Entry<Integer, ArrayList<String>> entry : treeMap.entrySet()) {
+            /* если значение get(1) из ArrayList уже есть в goodsList, тогда из goodsList получаем значения и суммируем с добавляемыми */
             if (goodsList.containsKey(entry.getValue().get(0))){
-                double sum = 0;
-                int count = 0;
-                for (SimpleEntry<Double, Integer> value : goodsList.values()) {
-                    sum = value.getKey();
-                    count = value.getValue();
-                }
+                AbstractMap.SimpleEntry<Double, Integer> simpleEntry = goodsList.get(entry.getValue().get(0));
+                Double sum = simpleEntry.getKey();
+                Integer count = simpleEntry.getValue();
                 goodsList.put(entry.getValue().get(0),
                         (new AbstractMap.SimpleEntry<>((Double.parseDouble(entry.getValue().get(3)) + sum),
                                 (Integer.parseInt(entry.getValue().get(2))) + count)));
