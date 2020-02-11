deaf07ec96bc0ced86856e03c2acef86c8ef5029
Oleg K <severniyman@gmail.com>
2020-02-11 19:41:59 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -2,13 +2,12 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class Insurance {
     public static enum FormatStyle {SHORT, LONG, FULL}
 
     private ZonedDateTime start;
-    private Duration duration;
+    private Duration duration; /* продолжительность действия */
 
     /* установить дату-время начала действия страховки */
     public Insurance(ZonedDateTime start) {
@@ -43,25 +42,23 @@ public class Insurance {
 
     /* установить продолжительность действия страховки */
     public void setDuration(Duration duration){
-        start.plus(duration);
-// сложить
+        this.duration = duration;
     }
     /* установить продолжительность действия страховки, задав дату-время окончания */
     public void setDuration(ZonedDateTime expiration){
-        start.minus(expiration);
-// старт и окончание
+        duration = Duration.between(expiration, start);
     }
     /* установить продолжительность действия страховки, задав целыми числами количество месяцев, дней и часов */
     public void setDuration(int months, int days, int hours){
-        start.plusMonths(months).plusDays(days).plusHours(hours);
-// к старту прибавить аргументы
+        ZonedDateTime endDate = start.plusMonths(months).plusDays(days).plusHours(hours);
+        setDuration(endDate);
     }
     /* установить продолжительность действия страховки
     * SHORT - целое число миллисекунд (тип long)
     * LONG  - ISO_LOCAL_DATE_TIME - как период, например “0000-06-03T10:00:00” означает, что продолжительность действия страховки 0 лет, 6 месяцев, 3 дня 10 часов.
     * FULL - стандартный формат Duration, который получается через toString() */
     public void setDuration(String strDuration, FormatStyle style){
-
+        
     }
     /* проверить действительна ли страховка на указанную дату-время. Если продолжительность не задана считать страховку бессрочной */
     public boolean checkValid(ZonedDateTime dateTime){
