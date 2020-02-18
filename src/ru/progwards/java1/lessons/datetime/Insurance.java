f58d65159ecdebbec4a6e5157baec389d0dde613
Oleg K <severniyman@gmail.com>
2020-02-18 15:53:34 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -90,9 +90,10 @@ public class Insurance {
         long longStart = start.toEpochSecond();
         long longDateTime = dateTime.toEpochSecond();
 
-        if (duration == null || duration.isNegative()){
+        if (duration == null){
+//        if (duration == null || duration.isNegative()){
             return longDateTime >= longStart;
-        } else if (longDateTime < longStart || longDateTime > (start.plus(duration)).toEpochSecond()){
+        } else if (longDateTime < longStart || longDateTime > (start.plus(duration)).toEpochSecond() || duration.isNegative()){
             return false;
         } else
         return longDateTime <= start.plus(duration).toEpochSecond();
