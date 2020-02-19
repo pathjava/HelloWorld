4136df6f3677c3425088d0f0ef83d51fa9d1e5a5
Oleg K <severniyman@gmail.com>
2020-02-19 20:50:18 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,25 +1,57 @@
 package ru.progwards.java1.lessons.datetime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Profiler {
+    public static ArrayList<StatisticInfo> listStatic = new ArrayList<>();
+
     /* войти в профилировочную секцию, замерить время входа */
-    public static void enterSection(String name){
+    public static void enterSection(String name) throws InterruptedException {
         long start = System.currentTimeMillis();
-        StatisticInfo startInfo = new StatisticInfo(name);
-        startInfo.setStartTime(start);
+        StatisticInfo statisticInfo = new StatisticInfo(name);
+        statisticInfo.setStartTime(start);
+        listStatic.add(statisticInfo);
+        Thread.sleep(500);
+        System.out.println(start);
     }
 
     /* выйти из профилировочной секции. Замерить время выхода, вычислить промежуток времени между входом и выходом в миллисекундах */
     public static void exitSection(String name){
         long end = System.currentTimeMillis();
-        StatisticInfo endInfo = new StatisticInfo(name);
-        endInfo.setEndTime(end);
+        StatisticInfo statisticInfo = null;
+        for (StatisticInfo info : listStatic) {
+            if (info.sectionName.equals(name)){
+                statisticInfo = info;
+            }
+        }
+        assert statisticInfo != null;
+        statisticInfo.setEndTime(end);
+        /* здесь надо - Замерить время выхода, вычислить промежуток времени между входом и выходом в миллисекундах*/
+        statisticInfo.setDuration(statisticInfo.getStartTime() - statisticInfo.getEndTime());
+        System.out.println(end);
     }
 
     /* получить профилировочную статистику, отсортировать по наименованию секции */
     public static List<StatisticInfo> getStatisticInfo(){
+        StatisticInfo listStat = new StatisticInfo("session1");
+        ArrayList<StatisticInfo> list = new ArrayList<>();
+        list.add(listStat);
+        return list;
+    }
 
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        enterSection("session1");
+        exitSection("session1");
+
+
+//        System.out.println(listTest);
+        for (StatisticInfo statisticInfo : listStatic) {
+            System.out.println(statisticInfo);
+        }
     }
 
 }
--- src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -6,7 +6,6 @@ public class StatisticInfo {
 
     public StatisticInfo(String name) {
         this.sectionName = name;
-        long startTime;
     }
 
     /* полное время выполнения секции в миллисекундах */
@@ -19,22 +18,40 @@ public class StatisticInfo {
     public int count;
 
 
-    private long startTime;
+    private long startTime = 0;
     private long endTime;
-
-    public long getStartTime() {
-        return startTime;
-    }
+    private long duration;
 
     public void setStartTime(long startTime) {
         this.startTime = startTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
     }
 
     public long getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(long endTime) {
-        this.endTime = endTime;
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "StatisticInfo{" +
+                "sectionName='" + sectionName + '\'' +
+                ", fullTime=" + fullTime +
+                ", selfTime=" + selfTime +
+                ", count=" + count +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", duration=" + duration +
+                '}';
     }
 }
