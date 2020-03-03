dc54d31cdb1f57e256cbd067b5207bcbbd0bfb87
Oleg K <severniyman@gmail.com>
2020-03-03 20:19:56 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -1,4 +1,32 @@
 package ru.progwards.java1.lessons.datetime;
 
+import java.util.List;
+
 public class SessionManager {
+    private List<UserSession> sessions;
+    private int sessionValid;
+
+    public SessionManager(int sessionValid) {
+        this.sessionValid = sessionValid;
+    }
+
+    public void add(UserSession userSession){
+
+    }
+
+//    public UserSession find(String userName){
+//
+//    }
+//
+//    public UserSession get(int sessionHandle){
+//
+//    }
+
+    public void delete(int sessionHandle){
+
+    }
+
+    public void deleteExpired(){
+
+    }
 }
--- src/ru/progwards/java1/lessons/datetime/UserSession.java
+++ src/ru/progwards/java1/lessons/datetime/UserSession.java
@@ -1,4 +1,63 @@
 package ru.progwards.java1.lessons.datetime;
 
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class UserSession {
+    private int sessionHandle;
+    private String userName;
+    private ZonedDateTime lastAccess;
+
+//    List<Integer> randomList = new ArrayList<>();
+
+    public UserSession(String userName) {
+//        Random random = new Random();
+//        sessionHandle = random.nextInt();
+//        for (int i = 1000; i < 9999; i++) {
+//            randomList.add(i);
+//        }
+//        Collections.shuffle(randomList);
+//        for (Integer integer : randomList) {
+//            sessionHandle = integer;
+//        }
+        sessionHandle = randomHandle();
+        this.userName = userName;
+        lastAccess = ZonedDateTime.now();
+    }
+
+    private int randomHandle(){
+        List<Integer> randomList = new ArrayList<>();
+        int randomInt = 0;
+        for (int i = 1000; i <= 9999; i++) {
+            randomList.add(i);
+        }
+        Collections.shuffle(randomList);
+        for (Integer integer : randomList) {
+            randomInt = integer;
+        }
+        return randomInt;
+    }
+
+    public void updateLastAccess(){
+        lastAccess = ZonedDateTime.now();
+    }
+
+    public int getSessionHandle() {
+        return sessionHandle;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public ZonedDateTime getLastAccess() {
+        return lastAccess;
+    }
+
+//    public static void main(String[] args) {
+//        UserSession userSession = new UserSession("Bob");
+//        System.out.println(userSession.sessionHandle);
+//    }
 }
