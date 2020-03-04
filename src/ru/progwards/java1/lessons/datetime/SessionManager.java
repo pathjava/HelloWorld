49ca1233fd372fec674e630f4ef03b103c273f5e
Oleg K <severniyman@gmail.com>
2020-03-04 18:55:27 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -2,6 +2,7 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class SessionManager {
@@ -10,39 +11,56 @@ public class SessionManager {
 
     public SessionManager(int sessionValid) {
         this.sessionValid = sessionValid;
+        sessions = new ArrayList<>();
     }
 
-    List<UserSession> listSessions = new ArrayList<>();
-    public void add(UserSession userSession){
-
+    public void add(UserSession userSession) {
+        sessions.add(userSession);
     }
 
-    public UserSession find(String userName){
-        for (UserSession session : listSessions) {
-            if (session.getUserName().equals(userName)){
-                ZonedDateTime currentTime = ZonedDateTime.now();
-                ZonedDateTime lastAccessTime = ZonedDateTime.from(session.getLastAccess().plusSeconds(sessionValid));
-                if (currentTime.isAfter(lastAccessTime) || session.getUserName() == null){
-                    return null;
-                } else {
-                    UserSession userSession = new UserSession(session.getUserName());
-                    userSession.updateLastAccess();
-                }
+    public UserSession find(String userName) {
+        for (UserSession session : sessions) {
+            ZonedDateTime currentTime = ZonedDateTime.now();
+            ZonedDateTime lastAccessTime = ZonedDateTime.from(session.getLastAccess().plusSeconds(sessionValid));
+            if ((session.getUserName().contains(userName) && currentTime.isAfter(lastAccessTime)) || session.getUserName() == null) {
+                return null;
+            } else if (session.getUserName().contains(userName) && currentTime.isBefore(lastAccessTime)){
+//                    UserSession userSession = new UserSession(session.getUserName());
+//                    userSession.updateLastAccess();
+                session.updateLastAccess();
             }
         }
     }
-//
-//    public UserSession get(int sessionHandle){
-//
-//    }
 
-    public void delete(int sessionHandle){
-
+    public UserSession get(int sessionHandle){
+        for (UserSession session : sessions) {
+            ZonedDateTime currentTime = ZonedDateTime.now();
+            ZonedDateTime lastAccessTime = ZonedDateTime.from(session.getLastAccess().plusSeconds(sessionValid));
+            if (((session.getSessionHandle() == sessionHandle) && currentTime.isAfter(lastAccessTime)) || session.getSessionHandle() == 0){
+                return null;
+            } else if (session.getSessionHandle() == sessionHandle && currentTime.isBefore(lastAccessTime)){
+                session.updateLastAccess();
+            }
+        }
     }
 
-    public void deleteExpired(){
-        ZonedDateTime currentTime = ZonedDateTime.now();
-//        ZonedDateTime lastAccessTime = ZonedDateTime.from()
+    public void delete(int sessionHandle) {
+        for (int i = 0; i < sessions.size(); i++) {
+            UserSession session = sessions.get(i);
+            if (session.getSessionHandle() == sessionHandle) {
+                sessions.remove(session);
+            }
+        }
+    }
+
+    public void deleteExpired() {
+        for (UserSession session : sessions) {
+            ZonedDateTime currentTime = ZonedDateTime.now();
+            ZonedDateTime lastAccessTime = ZonedDateTime.from(session.getLastAccess().plusSeconds(sessionValid));
+            if (currentTime.isAfter(lastAccessTime)){
+                sessions.remove(session);
+            }
+        }
     }
 
 
