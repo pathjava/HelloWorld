7b138e5289bc88d6648cf7ab8533a90461a12631
Oleg K <severniyman@gmail.com>
2020-03-05 09:44:27 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -4,7 +4,6 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class SessionManager {
     private List<UserSession> sessions;
