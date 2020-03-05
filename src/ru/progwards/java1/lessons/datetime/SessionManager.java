3099208c283c81e54f290133cf145ac0fec65730
Oleg K <severniyman@gmail.com>
2020-03-05 09:27:28 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -58,12 +58,6 @@ public class SessionManager {
     }
 
     public void delete(int sessionHandle) {
-//        for (int i = 0; i < sessions.size(); i++) {
-//            UserSession session = sessions.get(i);
-//            if (session.getSessionHandle() == sessionHandle) {
-//                sessions.remove(session);
-//            }
-//        }
         Iterator<UserSession> iterator = sessions.iterator();
         while (iterator.hasNext()){
             int handle = iterator.next().getSessionHandle();
@@ -74,14 +68,6 @@ public class SessionManager {
     }
 
     public void deleteExpired() {
-//        for (UserSession session : sessions) {
-//            ZonedDateTime currentTime = ZonedDateTime.now();
-//            ZonedDateTime lastAccessTime = ZonedDateTime.from(session.getLastAccess().plusSeconds(sessionValid));
-//            if (currentTime.isAfter(lastAccessTime)) {
-//                sessions.remove(session);
-//            }
-//        }
-
         Iterator<UserSession> iterator = sessions.iterator();
         while(iterator.hasNext()){
             ZonedDateTime currentTime = ZonedDateTime.now();
