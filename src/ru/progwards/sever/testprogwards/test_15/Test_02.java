3b805917178278df9bab9fd8e52e385f9f6b0cbe
Oleg K <severniyman@gmail.com>
2020-03-06 12:50:20 +0300

    Test 15

--- src/ru/progwards/sever/testprogwards/test_15/Test_02.java
+++ src/ru/progwards/sever/testprogwards/test_15/Test_02.java
@@ -14,17 +14,17 @@ public class Test_02 {
 
 
     public static void main(String[] args) {
-//        LocalDateTime ldt1= LocalDateTime.now();
-//        LocalDateTime ldt2= ldt1.plusDays(4);
-//        Duration duration = Duration.between(ldt1,ldt2);
-//        System.out.println(duration.toHours());
+        LocalDateTime ldt1= LocalDateTime.now();
+        LocalDateTime ldt2= ldt1.plusDays(4);
+        Duration duration = Duration.between(ldt1,ldt2);
+        System.out.println(duration.toHours());
 
 //        LocalDateTime ldt2= LocalDateTime.of(2019, 05, 05, 22, 24);
 //        System.out.println(ldt2);
-
+//
 //        ZoneId zid1 = ZoneId.of("Europe/Moscow");
 //        System.out.println(zid1.getRules().getOffset(Instant.now()));
-
-        System.out.println(createInstant());
+//
+//        System.out.println(createInstant());
     }
 }
