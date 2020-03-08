99365c1dd30697d7c3c20eda41a880bdd6eb769c
Oleg K <severniyman@gmail.com>
2020-03-08 12:26:05 +0300

    H15

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -386,3 +386,38 @@ public class Profiler {
     }
 
 }
+
+/* Решение, которое дал Сергей Алтунджи */
+//import java.time.Instant;
+//        import java.util.*;
+//
+//public class Profiler {
+//
+//    private static Deque<Long> sectionStartTimeDeque = new ArrayDeque<>();
+//    private static Deque<String> sectionNameDeque = new ArrayDeque<>();
+//    private static LinkedHashMap<String, StatisticInfo> sectionMap = new LinkedHashMap<>();
+//
+//    public static void enterSection(String name) {
+//        sectionMap.putIfAbsent(name, new StatisticInfo(name));
+//        sectionStartTimeDeque.push(Instant.now().toEpochMilli());
+//        sectionNameDeque.push(name);
+//    }
+//
+//    public static void exitSection(String name) {
+//        int period = (int)(Instant.now().toEpochMilli() - sectionStartTimeDeque.pop());
+//        sectionMap.get(name).count++;
+//        sectionMap.get(name).fullTime += period;
+//        sectionMap.get(name).selfTime += period;
+//        sectionNameDeque.pop();
+//        if(!sectionNameDeque.isEmpty()) {
+//            String externalSectionName = sectionNameDeque.peek();
+//            sectionMap.get(externalSectionName).selfTime -= period;
+//        }
+//    }
+//
+//    public static List<StatisticInfo> getStatisticInfo() {
+//        return new ArrayList<StatisticInfo>(sectionMap.values());
+//    }
+//
+//
+//}
