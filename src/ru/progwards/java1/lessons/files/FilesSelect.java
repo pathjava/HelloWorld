4ad32a88d6e8ed77859cbf924249d6196591adc4
Oleg K <severniyman@gmail.com>
2020-03-08 19:30:44 +0300

    H16

--- src/ru/progwards/java1/lessons/files/FilesSelect.java
+++ src/ru/progwards/java1/lessons/files/FilesSelect.java
@@ -1,4 +1,38 @@
 package ru.progwards.java1.lessons.files;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilesSelect {
+    public void selectFiles(String inFolder, String outFolder, List<String> keys) {
+        PathMatcher pathMatcher = FileSystems. getDefault().getPathMatcher( "glob:**/*.txt");
+        try {
+            Files.walkFileTree(Paths.get(inFolder), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    if (pathMatcher.matches(path))
+                    System.out.println(path);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException e) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FilesSelect test = new FilesSelect();
+        List<String> testList = new ArrayList<>(List.of("FindDuplicates", "test"));
+        test.selectFiles("C:\\Intellij Idea\\programming\\HelloWorld\\src\\ru\\progwards\\java1\\lessons\\files",
+                "C:\\Intellij Idea\\programming\\HelloWorld\\src\\ru\\progwards\\java1\\lessons\\files\\outFolder", testList);
+    }
 }
