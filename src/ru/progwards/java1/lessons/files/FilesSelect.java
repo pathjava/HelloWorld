ea51bfe6789cee4a4a109084f063a20d9d9c1ccd
Oleg K <severniyman@gmail.com>
2020-03-08 20:35:29 +0300

    H16

--- src/ru/progwards/java1/lessons/files/FilesSelect.java
+++ src/ru/progwards/java1/lessons/files/FilesSelect.java
@@ -8,13 +8,14 @@ import java.util.List;
 
 public class FilesSelect {
     public void selectFiles(String inFolder, String outFolder, List<String> keys) {
-        PathMatcher pathMatcher = FileSystems. getDefault().getPathMatcher( "glob:**/*.txt");
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.txt");
         try {
             Files.walkFileTree(Paths.get(inFolder), new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
                     if (pathMatcher.matches(path))
-                    System.out.println(path);
+                        checkFile(path, outFolder, keys);
+                        System.out.println(path);
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -25,6 +26,28 @@ public class FilesSelect {
             });
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void checkFile(Path path,String out, List<String> keys){
+        String fileContent = null;
+        try {
+            fileContent = Files.readString(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert fileContent != null;
+        for (String key : keys) {
+            if (fileContent.contains(key)){
+                Path directoryOut = Paths.get(out).resolve(key);
+                try {
+                    Files.createDirectory(directoryOut);
+                    Path destination = directoryOut.resolve(path.getFileName());
+                    Files.copy(path, destination, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
