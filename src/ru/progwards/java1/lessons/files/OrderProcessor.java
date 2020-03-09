826ace08a2b484665d4f5b87f7129fe00fc3787e
Oleg K <severniyman@gmail.com>
2020-03-09 20:21:14 +0300

    H16

--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -1,4 +1,12 @@
 package ru.progwards.java1.lessons.files;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class OrderProcessor {
+    Path startPath;
+
+    public OrderProcessor(String startPath) {
+        this.startPath = Paths.get(startPath);
+    }
 }
