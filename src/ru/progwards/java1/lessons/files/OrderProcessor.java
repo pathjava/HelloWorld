9fb7620d5f9a67b95193910bb8b84b307ae1016e
Oleg K <severniyman@gmail.com>
2020-03-09 20:51:39 +0300

    H16

--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -2,11 +2,16 @@ package ru.progwards.java1.lessons.files;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 
 public class OrderProcessor {
     Path startPath;
 
     public OrderProcessor(String startPath) {
         this.startPath = Paths.get(startPath);
+    }
+
+    public int loadOrders(LocalDate start, LocalDate finish, String shopId){
+        return 0;
     }
 }
