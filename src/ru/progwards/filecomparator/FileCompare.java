bce973d3ebdf714ea378ba5674976cf180b26994
Oleg K <severniyman@gmail.com>
2020-03-20 15:32:45 +0300

    File Comparator

--- src/ru/progwards/filecomparator/FileCompare.java
+++ src/ru/progwards/filecomparator/FileCompare.java
@@ -12,16 +12,26 @@ public class FileCompare {
     List<String> listOne = new ArrayList<>();
     List<String> listTwo = new ArrayList<>();
 
-    public void readFiles(String pathOne, String pathTwo){
-        Path pathFileOne = Paths.get(pathOne);
-        Path pathFileTwo = Paths.get(pathTwo);
+    public void readFiles(String pathOne, String pathTwo) {
+        Path pathFileOne = null;
+        Path pathFileTwo = null;
+        if (pathOne != null)
+            pathFileOne = Paths.get(pathOne);
+        else
+            System.out.println("Файл 1 не выбран!");
+        if (pathTwo != null)
+            pathFileTwo = Paths.get(pathTwo);
+        else
+            System.out.println("Файл 2 не выбран!");
 
         try {
+            assert pathFileOne != null;
             listOne = Files.readAllLines(pathFileOne);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
+            assert pathFileTwo != null;
             listTwo = Files.readAllLines(pathFileTwo);
         } catch (IOException e) {
             e.printStackTrace();
