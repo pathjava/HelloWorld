f74f2e196c138167d4f1122e11ec7db2aa97fdba
Oleg K <severniyman@gmail.com>
2020-03-29 20:26:47 +0300

    File Comparator

--- src/ru/progwards/filecomparator/FileCompareAnchors.java
+++ src/ru/progwards/filecomparator/FileCompareAnchors.java
@@ -5,20 +5,20 @@ public class FileCompareAnchors {
     public int startLineBefore;
     public int finishLineBefore;
 
-//    @Override
-//    public String toString() {
-//        if (startLineBefore == 0 && finishLineBefore == 0)
-//            return "|   |   " + String.format("| %s", lineFromFile);
-//        else if (finishLineBefore == 0)
-//            return "|" + String.format("%3d|", startLineBefore) + "   " + String.format("| %s", lineFromFile);
-//        else if (startLineBefore == 0)
-//            return "|   |" + String.format("%3d| %s", finishLineBefore, lineFromFile);
-//        else
-//            return "|" + String.format("%3d|%3d| %s", startLineBefore, finishLineBefore, lineFromFile);
-//    }
-
     @Override
     public String toString() {
-        return "| " + lineFromFile;
+        if (startLineBefore == 0 && finishLineBefore == 0)
+            return "|   |   " + String.format("| %s", lineFromFile);
+        else if (finishLineBefore == 0)
+            return "|" + String.format("%3d|", startLineBefore) + "   " + String.format("| %s", lineFromFile);
+        else if (startLineBefore == 0)
+            return "|   |" + String.format("%3d| %s", finishLineBefore, lineFromFile);
+        else
+            return "|" + String.format("%3d|%3d| %s", startLineBefore, finishLineBefore, lineFromFile);
     }
+
+//    @Override
+//    public String toString() {
+//        return "| " + lineFromFile;
+//    }
 }
