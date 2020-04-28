655c5299eabd1760dd329813c8bc888bb94ceb8b
Oleg K <severniyman@gmail.com>
2020-04-28 20:30:46 +0300

    File Comparator

---
+++ src/ru/progwards/filecomparator/PatchLines.java
@@ -0,0 +1,15 @@
+// Oleg Kiselev
+// 28.04.2020, 20:04
+
+package ru.progwards.filecomparator;
+
+public class PatchLines {
+    public String dogs = "";
+    public int oneStart;
+    public int oneCount;
+    public int twoStart;
+    public int twoCount;
+    public String noNewLine = "";
+    public String plusMinusEmpty = "";
+    public String fileLines = "";
+}
