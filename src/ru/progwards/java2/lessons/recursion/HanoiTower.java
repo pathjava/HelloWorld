1bce842ce281412d16d56d05f0b8e93223ebfaff
Oleg K <severniyman@gmail.com>
2020-05-12 20:42:30 +0300

    recursion and lambda

--- src/ru/progwards/java2/lessons/recursion/HanoiTower.java
+++ src/ru/progwards/java2/lessons/recursion/HanoiTower.java
@@ -13,17 +13,39 @@ public class HanoiTower {
         this.pos = pos;
     }
 
-    // везде данную задачу решают через void move(int n, int a, int b, int c) и это логично, три столба и количество дисков
-    // но в данном случае void move(int from, int to) - ничего не понимаю...
-    public void move(int from, int to){
+    public void move(int from, int to) {
+        int middle;
+        if (to == 2)
+            middle = 3;
+        else {
+            to = 3;
+            middle = 2;
+        }
+        hanoi(size, from, to, middle);
+    }
+
+    public void hanoi(int n, int from, int to, int middle) {
+        if (n == 0)
+            return;
+
+        hanoi(n - 1, from, middle, to);
+
+        System.out.println(from + " " + to);
+
+        hanoi(n - 1, middle, to, from);
+    }
+
+    public void print() {
+        System.out.println();
+    }
+
+    public void setTrace(boolean on) {
 
     }
 
-    public void print(){
 
-    }
-
-    public void setTrace(boolean on){
-
+    public static void main(String[] args) {
+        HanoiTower hanoiTower = new HanoiTower(3, 0);
+        hanoiTower.move(1, 2);
     }
 }
