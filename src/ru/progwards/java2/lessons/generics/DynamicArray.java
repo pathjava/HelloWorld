da98989e64595ba8e34d2a754c7be56387b2ad34
Oleg K <severniyman@gmail.com>
2020-05-13 20:14:47 +0300

    generics

--- src/ru/progwards/java2/lessons/generics/DynamicArray.java
+++ src/ru/progwards/java2/lessons/generics/DynamicArray.java
@@ -6,7 +6,7 @@ package ru.progwards.java2.lessons.generics;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class DynamicArray<T extends Number> {
+public class DynamicArray<T> {
 
     private T[] arr;
     private int size;
@@ -58,11 +58,11 @@ public class DynamicArray<T extends Number> {
         }
     }
 
-    private void get(int pos){
+    private void get(int pos) {
         System.out.println(arr[pos]);
     }
 
-    private void size(){
+    private void size() {
         System.out.println(realSize());
     }
 
@@ -70,6 +70,12 @@ public class DynamicArray<T extends Number> {
     public static void main(String[] args) {
         DynamicArray<Integer> dynamicArray = new DynamicArray<>();
         dynamicArray.arr = new Integer[]{5, 1, 14, 34, 22, 3, 1, 3, 100, 17};
+
+        /*
+        * со строками работает, только в тестовых методах надо к i + "строка"
+        * */
+//        DynamicArray<String> dynamicArray = new DynamicArray<>();
+//        dynamicArray.arr = new String[]{"Михаил", "Алексей", "Сергей", "Игорь", "Даша", "Лена", "Bill", "Mike", "Alex"};
 
         for (int i = 1; i <= 10; i++) {
             dynamicArray.add(i);
@@ -88,9 +94,5 @@ public class DynamicArray<T extends Number> {
         dynamicArray.size();
 
         System.out.println(Arrays.toString(dynamicArray.arr));
-
-//        DynamicArray<String> dynamicArray1 = new DynamicArray<>();
-//        dynamicArray1.arr = new String[]{"Михаил", "Алексей", "Сергей", "Игорь", "Даша", "Лена", "Bill", "Mike", "Alex"};
     }
-
 }
