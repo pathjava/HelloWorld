2ce24c06c8b71cda975ff2ac6547d140e6908501
Oleg K <severniyman@gmail.com>
2020-05-14 15:18:08 +0300

    test3

--- src/ru/progwards/java2/lessons/generics/FruitBox.java
+++ src/ru/progwards/java2/lessons/generics/FruitBox.java
@@ -8,28 +8,28 @@ import java.util.List;
 
 public class FruitBox<T extends Fruit> extends ArrayList<T> {
 
-    private List<T> fruitList;
-
-    private void addFruit(List<? extends Fruit> list){
-        fruitList.addAll(list);
-    }
-
-
-    public static void main(String[] args) {
-        FruitBox<Fruit> fruitBox = new FruitBox<>();
-
-        List<Fruit> list = List.of(
-                new Apple(1.0),
-                new Apple(1.0),
-                new Apple(1.0),
-                new Orange(1.5),
-                new Apple(1.0),
-                new Apple(1.0),
-                new Apple(1.0),
-                new Apple(1.0)
-        );
-
-        fruitBox.addFruit(list);
-    }
+//    private List<T> fruitList;
+//
+//    private void addFruit(List<? extends Fruit> list){
+//        fruitList.addAll(list);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        FruitBox<Fruit> fruitBox = new FruitBox<>();
+//
+//        List<Fruit> list = List.of(
+//                new Apple(1.0),
+//                new Apple(1.0),
+//                new Apple(1.0),
+//                new Orange(1.5),
+//                new Apple(1.0),
+//                new Apple(1.0),
+//                new Apple(1.0),
+//                new Apple(1.0)
+//        );
+//
+//        fruitBox.addFruit(list);
+//    }
 
 }
