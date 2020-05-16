a5be452ed4ee37dbb05288c372ec1b03d0f92221
Oleg K <severniyman@gmail.com>
2020-05-16 15:22:51 +0300

    basetypes

---
+++ src/ru/progwards/java2/lessons/basetypes/BiDirList.java
@@ -0,0 +1,93 @@
+// Oleg Kiselev
+// 15.05.2020, 19:42
+
+package ru.progwards.java2.lessons.basetypes;
+
+
+public class BiDirList<T> {
+
+    private int size = 0;
+
+    class ListItem<T> {
+
+        private T item;
+        private ListItem<T> next;
+        private ListItem<T> prev;
+
+        public ListItem(T item) {
+            this.item = item;
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        public void setNext(ListItem<T> item) {
+            next = item;
+        }
+
+        public ListItem<T> getNext() {
+            return next;
+        }
+
+        public void setPrev(ListItem<T> prev) {
+            this.prev = prev;
+        }
+
+        public ListItem<T> getPrev() {
+            return prev;
+        }
+    }
+
+    private ListItem<T> head;
+    private ListItem<T> tail;
+
+    ListItem<T> getHead() {
+        return head;
+    }
+
+    public void addLast(T item) {
+        final ListItem<T> li = new ListItem<>(item);
+        if (head == null) {
+            head = li;
+        } else {
+            tail.setNext(li);
+            head.setPrev(li);
+        }
+        tail = li;
+        size++;
+    }
+
+    public void addFirst(T item) {
+        final ListItem<T> li = new ListItem<>(item);
+        if (tail == null) {
+            tail = li;
+        } else {
+            head.setPrev(li);
+        }
+        head = li;
+        size++;
+    }
+
+
+    public static void main(String[] args) {
+        BiDirList<Integer> list = new BiDirList<>();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+
+        list.addFirst(4);
+        list.addFirst(5);
+        list.addFirst(6);
+
+//        BiDirList<Integer>.ListItem<Integer> current = list.getHead();
+//        while (current != null) {
+//            System.out.println(current.getItem());
+//            current = current.getNext();
+//        }
+
+
+        System.out.println("Size = " + list.size);
+    }
+
+}
