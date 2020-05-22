a7aa9745503aee4954d64ad90fb7c19384255e4c
Oleg K <severniyman@gmail.com>
2020-05-22 21:07:23 +0300

    basetypes

--- src/ru/progwards/java2/lessons/basetypes/IntegerHashValue.java
+++ src/ru/progwards/java2/lessons/basetypes/IntegerHashValue.java
@@ -7,17 +7,20 @@ public class IntegerHashValue implements HashValue {
 
     public int value;
 
-    DoubleHashTable.ItemHashTable temp;
-
     public IntegerHashValue(int value) {
         this.value = value;
     }
 
     @Override
     public int getHash() {
-        int hashCode = (int) temp.getKey();
+        int hashCode = value;
         hashCode ^= (hashCode >>> 20) ^ (hashCode >>> 12);
         hashCode = hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);
         return hashCode;
+    }
+
+    @Override
+    public String toString() {
+        return "" + value;
     }
 }
