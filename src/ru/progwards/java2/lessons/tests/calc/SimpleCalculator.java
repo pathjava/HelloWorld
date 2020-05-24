5fa94389249cc053b297af5b813c28113fdd7c61
Oleg K <severniyman@gmail.com>
2020-05-24 19:15:54 +0300

    tests

--- src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
+++ src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
@@ -5,24 +5,41 @@ package ru.progwards.java2.lessons.tests.calc;
 
 public class SimpleCalculator implements SimpleCalculatorInterface {
 
+    private boolean checkMaxOrMin(int val1, int val2){
+        if (val1 == Integer.MAX_VALUE || val1 == Integer.MIN_VALUE || val2 == Integer.MAX_VALUE || val2 == Integer.MIN_VALUE)
+            return false;
+        return true;
+    }
 
     @Override
     public int sum(int val1, int val2) {
-        return 0;
+        int result = 0;
+        if (checkMaxOrMin(val1, val2))
+            result = val1 + val2;
+        return result;
     }
 
     @Override
     public int diff(int val1, int val2) {
-        return 0;
+        int result = 0;
+        if (checkMaxOrMin(val1, val2))
+            result = val1 - val2;
+        return result;
     }
 
     @Override
     public int mult(int val1, int val2) {
-        return 0;
+        int result = 0;
+        if (checkMaxOrMin(val1, val2))
+            result = val1 * val2;
+        return result;
     }
 
     @Override
     public int div(int val1, int val2) {
-        return 0;
+        int result = 0;
+        if (checkMaxOrMin(val1, val2) && val2 != 0)
+            result = val1 / val2;
+        return result;
     }
 }
--- src/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
+++ src/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
@@ -3,25 +3,35 @@
 
 package ru.progwards.java2.lessons.tests.test.calc;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import ru.progwards.java2.lessons.tests.calc.SimpleCalculator;
 
 import static org.junit.Assert.*;
 
 public class SimpleCalculatorTest {
 
-    @Test
-    public void sum() {
+    private int val1 = 5;
+    private int val2 = 7;
+
+    @BeforeClass
+    public static void createInstance(){
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
     }
 
     @Test
-    public void diff() {
+    public void sumTestMethod() {
     }
 
     @Test
-    public void mult() {
+    public void diffTestMethod() {
     }
 
     @Test
-    public void div() {
+    public void multTestMethod() {
+    }
+
+    @Test
+    public void divTestMethod() {
     }
 }
