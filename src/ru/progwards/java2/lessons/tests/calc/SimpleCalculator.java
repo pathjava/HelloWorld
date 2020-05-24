dd1de076cdf960b4ed944f7a865fa40aff54799c
Oleg K <severniyman@gmail.com>
2020-05-24 19:38:05 +0300

    tests

--- src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
+++ src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
@@ -6,40 +6,38 @@ package ru.progwards.java2.lessons.tests.calc;
 public class SimpleCalculator implements SimpleCalculatorInterface {
 
     private boolean checkMaxOrMin(int val1, int val2){
-        if (val1 == Integer.MAX_VALUE || val1 == Integer.MIN_VALUE || val2 == Integer.MAX_VALUE || val2 == Integer.MIN_VALUE)
+        if (val1 > Integer.MAX_VALUE || val1 < Integer.MIN_VALUE || val2 > Integer.MAX_VALUE || val2 < Integer.MIN_VALUE)
             return false;
         return true;
     }
 
     @Override
     public int sum(int val1, int val2) {
-        int result = 0;
-        if (checkMaxOrMin(val1, val2))
-            result = val1 + val2;
-        return result;
+        if (!checkMaxOrMin(val1, val2))
+            throw new IllegalArgumentException();
+        return val1 + val2;
     }
 
     @Override
     public int diff(int val1, int val2) {
-        int result = 0;
-        if (checkMaxOrMin(val1, val2))
-            result = val1 - val2;
-        return result;
+        if (!checkMaxOrMin(val1, val2))
+            throw new IllegalArgumentException();
+        return val1 - val2;
     }
 
     @Override
     public int mult(int val1, int val2) {
-        int result = 0;
-        if (checkMaxOrMin(val1, val2))
-            result = val1 * val2;
-        return result;
+        if (!checkMaxOrMin(val1, val2))
+            throw new IllegalArgumentException();
+        return val1 * val2;
     }
 
     @Override
     public int div(int val1, int val2) {
-        int result = 0;
-        if (checkMaxOrMin(val1, val2) && val2 != 0)
-            result = val1 / val2;
-        return result;
+        if (!checkMaxOrMin(val1, val2))
+            throw new IllegalArgumentException();
+        if (val2 == 0)
+            throw new ArithmeticException();
+        return val1 / val2;
     }
 }
--- src/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
+++ src/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
@@ -13,25 +13,45 @@ public class SimpleCalculatorTest {
 
     private int val1 = 5;
     private int val2 = 7;
+    public static SimpleCalculator simpleCalculator;
 
     @BeforeClass
-    public static void createInstance(){
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
+    public static void createInstance() {
+        simpleCalculator = new SimpleCalculator();
     }
 
     @Test
     public void sumTestMethod() {
+        int actual = simpleCalculator.sum(7, 5);
+        int expected = 12;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void diffTestMethod() {
+        int actual = simpleCalculator.diff(7, 5);
+        int expected = 2;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void multTestMethod() {
+        int actual = simpleCalculator.mult(7, 5);
+        int expected = 35;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void divTestMethod() {
+        int actual = simpleCalculator.div(7, 5);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divTestByZero() {
+        int actual = simpleCalculator.div(7, 0);
+        int expected = 1;
+        assertEquals(expected, actual);
     }
 }
