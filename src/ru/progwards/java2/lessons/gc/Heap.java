d4ea4361490fe79b9ab31cc46056fee4846968e0
Oleg K <severniyman@gmail.com>
2020-05-27 12:42:30 +0300

    tests

---
+++ src/ru/progwards/java2/lessons/gc/Heap.java
@@ -0,0 +1,18 @@
+// Oleg Kiselev
+// 27.05.2020, 12:38
+
+package ru.progwards.java2.lessons.gc;
+
+public class Heap {
+
+    private byte[] bytes;
+
+    public Heap(int maxHeapSize) {
+        bytes = new byte[maxHeapSize];
+    }
+
+    public int malloc(int size){
+
+    }
+
+}
