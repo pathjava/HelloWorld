157fb1c3c0d5ab81d1c15e549d446d9ed48adf10
Oleg K <severniyman@gmail.com>
2020-06-03 17:41:05 +0300

    gc

--- src/ru/progwards/java2/lessons/reflection/ClassInspector.java
+++ src/ru/progwards/java2/lessons/reflection/ClassInspector.java
@@ -5,23 +5,44 @@ package ru.progwards.java2.lessons.reflection;
 
 
 import java.lang.reflect.Modifier;
-import java.lang.reflect.Type;
 
 public class ClassInspector {
 
+    public static StringBuilder stringBuilder = new StringBuilder();
+
     public static void inspect(String clazz) throws ClassNotFoundException {
 
-        Class inspectedClass = Class.forName(clazz);
+        Class<?> inspectedClass = Class.forName(clazz);
+//        int mod = inspectedClass.getModifiers();
 
-        int mod = inspectedClass.getModifiers();
+        printClass(inspectedClass);
 
-        System.out.println(Modifier.isInterface(mod));
+//        String classOrInterface = Modifier.isInterface(mod) ? "interface " : "class ";
+//        System.out.println(classOrInterface);
 
-        System.out.println(inspectedClass.getSimpleName());
 
-        int modifierClass = inspectedClass.getModifiers();
-        String modStr = Modifier.toString(modifierClass);
+
+//        System.out.println(inspectedClass.getSimpleName());
+//
+//        int modifierClass = inspectedClass.getModifiers();
+//        String modStr = Modifier.toString(modifierClass);
     }
+
+    private static void printClass(Class<?> inspectedClass){
+        int mod = inspectedClass.getModifiers();
+        checkClassOrInterface(mod);
+        System.out.print(inspectedClass.getSimpleName() + " {");
+    }
+
+    private static void checkClassOrInterface(int mod){
+        stringBuilder.append(Modifier.isInterface(mod) ? "interface " : "class ");
+        System.out.print(stringBuilder.toString());
+    }
+
+
+
+
+
 
 
     public static void main(String[] args) throws ClassNotFoundException {
