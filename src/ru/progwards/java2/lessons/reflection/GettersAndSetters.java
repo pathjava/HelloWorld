c4c62508c8e0439c489b91d3dbac038632f0d548
Oleg K <severniyman@gmail.com>
2020-06-04 20:24:23 +0300

    reflection

--- src/ru/progwards/java2/lessons/reflection/GettersAndSetters.java
+++ src/ru/progwards/java2/lessons/reflection/GettersAndSetters.java
@@ -3,11 +3,14 @@
 
 package ru.progwards.java2.lessons.reflection;
 
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class GettersAndSetters {
@@ -24,19 +27,29 @@ public class GettersAndSetters {
     private static void checkFields(Class<?> clazz) {
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
-            listFields.add(field.getName());
+            String tempName = field.getName();
+            String getName = "get" + tempName.substring(0, 1).toUpperCase() + tempName.substring(1);
+            listFields.add(getName);
+            String setName = "set" + tempName.substring(0, 1).toUpperCase() + tempName.substring(1);
+            listFields.add(setName);
         }
     }
 
     private static void checkGettersAndSetters(Class<?> clazz) {
         StringBuilder builder = new StringBuilder();
         Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            int mod = method.getModifiers();
-            String modStr = checkModifiers(mod);
-            String typeStr = method.getReturnType().getSimpleName();
-            String nameStr = method.getName();
-            String paramStr = checkParameters(method.getParameters());
+        List<Method> listMethods = new ArrayList<>(Arrays.asList(methods));
+
+        for (Iterator<String> it = listFields.iterator(); it.hasNext(); ) {
+            String listField = it.next();
+            for (Method listMethod : listMethods) {
+                String nameStr = listMethod.getName();
+                if (listField.equals(nameStr))
+                    it.remove();
+            }
+        }
+        for (String listField : listFields) {
+            System.out.println(listField);
         }
     }
 
