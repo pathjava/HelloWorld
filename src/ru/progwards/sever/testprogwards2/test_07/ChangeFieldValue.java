e1258a8df9319ea9e829f7e5e47a99c74e9fe9b9
Oleg K <severniyman@gmail.com>
2020-06-05 15:42:14 +0300

    test07

--- src/ru/progwards/sever/testprogwards2/test_07/ChangeFieldValue.java
+++ src/ru/progwards/sever/testprogwards2/test_07/ChangeFieldValue.java
@@ -3,10 +3,7 @@
 
 package ru.progwards.sever.testprogwards2.test_07;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public class ChangeFieldValue {
 
