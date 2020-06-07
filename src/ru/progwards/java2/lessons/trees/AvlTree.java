2aa8f44a56b6d21f5b1792413be6ec0300aa0675
Oleg K <severniyman@gmail.com>
2020-06-07 19:58:56 +0300

    trees

---
+++ src/ru/progwards/java2/lessons/trees/AvlTree.java
@@ -0,0 +1,78 @@
+// Oleg Kiselev
+// 07.06.2020, 19:30
+
+package ru.progwards.java2.lessons.trees;
+
+public class AvlTree<K, V> {
+
+    private Node<K, V> root;
+    private int size = 0;
+
+    public class Node<K, V> {
+        private int height;
+        private K key;
+        private V value;
+        private Node<K, V> parent;
+        private Node<K, V> left;
+        private Node<K, V> right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Node<K, V> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<K, V> parent) {
+            this.parent = parent;
+        }
+
+        public Node<K, V> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<K, V> left) {
+            this.left = left;
+        }
+
+        public Node<K, V> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<K, V> right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "key=" + key + ", value=" + value;
+        }
+    }
+
+}
