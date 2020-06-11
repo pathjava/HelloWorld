c88527921346e0d3faec4a3cfa6006e9e50c81d8
Oleg K <severniyman@gmail.com>
2020-06-11 20:46:25 +0300

    trees

--- src/ru/progwards/java2/lessons/trees/AvlTree.java
+++ src/ru/progwards/java2/lessons/trees/AvlTree.java
@@ -78,7 +78,7 @@ public class AvlTree<K extends Comparable<K>, V> {
         }
     }
 
-    public void delete(K key) { // TODO - добавить size-- после удаления узла
+    public void delete(K key) {
         if (key == null)
             throw new IllegalArgumentException("The key cannot be null!");
         if (root == null)
@@ -86,24 +86,25 @@ public class AvlTree<K extends Comparable<K>, V> {
 
         Node<K, V> node = root;
         searchNode(node, key);
+        size--;
     }
 
     private void searchNode(Node<K, V> node, K key) {
-        Node<K, V> newNode;
+        Node<K, V> tempNode;
         if (key.compareTo(node.key) < 0) {
             if (node.left == null)
                 throw new IllegalArgumentException("This key is not exist!");
-            newNode = node.left;
+            tempNode = node.left;
         } else if (key.compareTo(node.key) > 0) {
             if (node.right == null)
                 throw new IllegalArgumentException("This key is not exist!");
-            newNode = node.right;
+            tempNode = node.right;
         } else
-            newNode = root;
-        if (key.compareTo(newNode.key) == 0)
-            rebuildNodes(newNode);
+            tempNode = root;
+        if (key.compareTo(tempNode.key) == 0)
+            rebuildNodes(tempNode);
         else
-            searchNode(newNode, key);
+            searchNode(tempNode, key);
     }
 
     private void rebuildNodes(Node<K, V> node) {
@@ -116,56 +117,56 @@ public class AvlTree<K extends Comparable<K>, V> {
     }
 
     private void rebuildNodesLeft(Node<K, V> node) {
-        Node<K, V> newNode;
-        newNode = searchMaxKey(node.left);
-        node.key = newNode.key;
-        node.value = newNode.value;
-        if (newNode.left != null) {
-            if (newNode.left.key.compareTo(newNode.parent.key) > 0)
-                newNode.parent.right = newNode.left;
+        Node<K, V> tempNode;
+        tempNode = searchMaxKey(node.left);
+        node.key = tempNode.key;
+        node.value = tempNode.value;
+        if (tempNode.left != null) {
+            if (tempNode.left.key.compareTo(tempNode.parent.key) > 0)
+                tempNode.parent.right = tempNode.left;
             else
-                newNode.parent.left = newNode.left;
-            newNode.left.parent = newNode.parent;
-        } else if (newNode.right == null)
-            newNode.parent.left = null;
+                tempNode.parent.left = tempNode.left;
+            tempNode.left.parent = tempNode.parent;
+        } else if (tempNode.right == null)
+            tempNode.parent.left = null;
         else
-            newNode.parent.right = null;
+            tempNode.parent.right = null;
     }
 
     private void rebuildNodesRight(Node<K, V> node) {
-        Node<K, V> newNode;
-        newNode = searchMinKey(node.right);
-        node.key = newNode.key;
-        node.value = newNode.value;
-        if (newNode.right != null) {
-            if (newNode.right.key.compareTo(newNode.parent.key) < 0)
-                newNode.parent.left = newNode.right;
+        Node<K, V> tempNode;
+        tempNode = searchMinKey(node.right);
+        node.key = tempNode.key;
+        node.value = tempNode.value;
+        if (tempNode.right != null) {
+            if (tempNode.right.key.compareTo(tempNode.parent.key) < 0)
+                tempNode.parent.left = tempNode.right;
             else
-                newNode.parent.right = newNode.right;
-            newNode.right.parent = newNode.parent;
-        } else if (newNode.left == null)
-            newNode.parent.right = null;
+                tempNode.parent.right = tempNode.right;
+            tempNode.right.parent = tempNode.parent;
+        } else if (tempNode.left == null)
+            tempNode.parent.right = null;
         else
-            newNode.parent.left = null;
+            tempNode.parent.left = null;
     }
 
     private Node<K, V> searchMaxKey(Node<K, V> node) {
-        Node<K, V> newNode;
+        Node<K, V> tempNode;
         if (node.right == null)
             return node;
         else {
-            newNode = node.right;
-            return searchMaxKey(newNode);
+            tempNode = node.right;
+            return searchMaxKey(tempNode);
         }
     }
 
     private Node<K, V> searchMinKey(Node<K, V> node) {
-        Node<K, V> newNode;
+        Node<K, V> tempNode;
         if (node.left == null)
             return node;
         else {
-            newNode = node.left;
-            return searchMinKey(newNode);
+            tempNode = node.left;
+            return searchMinKey(tempNode);
         }
     }
 
@@ -186,36 +187,34 @@ public class AvlTree<K extends Comparable<K>, V> {
             throw new IllegalArgumentException("The key cannot be null!");
         if (root == null)
             throw new IllegalArgumentException("AVL Tree is empty!");
-        if (key.compareTo(root.key) == 0)
-            return root.value;
         Node<K, V> node = root;
-        Node<K, V> newNode = keyAndValueSearch(node, key);
-        return newNode.value;
+        Node<K, V> tempNode = keyAndValueSearchFromFind(node, key);
+        return tempNode.value;
     }
 
-    private Node<K, V> keyAndValueSearch(Node<K, V> node, K key) {
-        Node<K, V> newNode;
-        int comparision = key.compareTo(node.key);
-        if (comparision < 0) {
+    private Node<K, V> keyAndValueSearchFromFind(Node<K, V> node, K key) {
+        Node<K, V> tempNode;
+        if (key.compareTo(node.key) < 0) {
             if (node.left == null)
                 throw new IllegalArgumentException("Key not found!");
             else if (key.compareTo(node.left.key) == 0)
-                newNode = node.left;
+                tempNode = node.left;
             else {
-                newNode = node.left;
-                return keyAndValueSearch(newNode, key);
+                tempNode = node.left;
+                return keyAndValueSearchFromFind(tempNode, key);
             }
-        } else {
+        } else if (key.compareTo(node.key) > 0) {
             if (node.right == null)
                 throw new IllegalArgumentException("Key not found!");
             else if (key.compareTo(node.right.key) == 0)
-                newNode = node.right;
+                tempNode = node.right;
             else {
-                newNode = node.right;
-                return keyAndValueSearch(newNode, key);
+                tempNode = node.right;
+                return keyAndValueSearchFromFind(tempNode, key);
             }
-        }
-        return newNode;
+        } else
+            return root;
+        return tempNode;
     }
 
     public void change(K oldKey, K newKey) {
@@ -229,40 +228,38 @@ public class AvlTree<K extends Comparable<K>, V> {
             throw new IllegalArgumentException("The key cannot be null!");
         if (root == null)
             throw new IllegalArgumentException("AVL Tree is empty!");
-        if (key.compareTo(root.key) == 0) // TODO - подумать и перенести это в основной метод замены значения
-            root.value = newValue;
         Node<K, V> node = root;
         replacementValueFromChangeValue(node, key, newValue);
     }
 
     private void replacementValueFromChangeValue(Node<K, V> node, K key, V newValue) {
-        Node<K, V> newNode;
-        int comparision = key.compareTo(node.key);
-        if (comparision < 0) {
+        Node<K, V> tempNode;
+        if (key.compareTo(node.key) < 0) {
             if (node.left == null)
                 throw new IllegalArgumentException("Key not found!");
             else if (key.compareTo(node.left.key) == 0)
                 node.left.value = newValue;
             else {
-                newNode = node.left;
-                replacementValueFromChangeValue(newNode, key, newValue);
+                tempNode = node.left;
+                replacementValueFromChangeValue(tempNode, key, newValue);
             }
-        } else {
+        } else if (key.compareTo(node.key) > 0) {
             if (node.right == null)
                 throw new IllegalArgumentException("Key not found!");
             else if (key.compareTo(node.right.key) == 0)
                 node.right.value = newValue;
             else {
-                newNode = node.right;
-                replacementValueFromChangeValue(newNode, key, newValue);
+                tempNode = node.right;
+                replacementValueFromChangeValue(tempNode, key, newValue);
             }
-        }
+        } else
+            root.value = newValue;
     }
 
 
     public static void main(String[] args) {
         AvlTree<Integer, String> test = new AvlTree<>();
-        test.put(21, "***");
+        test.put(21, "*21*");
         test.put(13, "***");
         test.put(29, "***");
         test.put(8, "***");
@@ -275,14 +272,14 @@ public class AvlTree<K extends Comparable<K>, V> {
         test.put(20, "***");
         test.put(24, "***");
         test.put(28, "***");
-        test.put(31, "***");
+        test.put(31, "*31*");
         test.put(33, "***");
         test.put(3, "***");
         test.put(7, "***");
         test.put(10, "***");
         test.put(12, "***");
         test.put(15, "***");
-        test.put(17, "***");
+        test.put(17, "*17*");
         test.put(19, "***");
         test.put(23, "***");
         test.put(25, "***");
@@ -298,11 +295,11 @@ public class AvlTree<K extends Comparable<K>, V> {
 
         test.change(11, 34);
 
+        System.out.println(test.find(31));
         test.delete(21);
 
-//        System.out.println(test.find(26));
-//        System.out.println(test.size);
-//        test.changeValue(26, "*new-26*");
+        System.out.println(test.size);
+        test.changeValue(20, "*20*");
     }
 
 }
