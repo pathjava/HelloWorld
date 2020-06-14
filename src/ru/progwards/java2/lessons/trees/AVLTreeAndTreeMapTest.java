2dea3538c88138668ff29dbf30e9907d5a0db7ba
Oleg K <severniyman@gmail.com>
2020-06-14 12:37:12 +0300

    trees

--- src/ru/progwards/java2/lessons/trees/AVLTreeAndTreeMapTest.java
+++ src/ru/progwards/java2/lessons/trees/AVLTreeAndTreeMapTest.java
@@ -3,28 +3,69 @@
 
 package ru.progwards.java2.lessons.trees;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 public class AVLTreeAndTreeMapTest {
-    private static final AvlTree<Integer, Integer> avlTree = new AvlTree<>();
-    private static final Map<Integer,Integer> treeMap = new TreeMap<>();
+    private static final AvlTree<Integer, Integer> avlTreeNumbers = new AvlTree<>();
+    private static final Map<Integer, Integer> treeMapNumbers = new TreeMap<>();
+
+    private static final AvlTree<Integer, String> avlTreeStrings = new AvlTree<>();
+    private static final Map<Integer, String> treeMapStrings = new TreeMap<>();
 
     private static final List<Integer> sortedNumbers = new ArrayList<>();
     private static final List<Integer> randomNumbers = new ArrayList<>();
-    private static List<String> tokensList;
-    static {
+    private static List<String> tokensList = new ArrayList<>();
+
+    public static void fillingSortedData() {
+        IntStream.range(0, 1000000).forEachOrdered(sortedNumbers::add);
+        IntStream.range(0, 1000000).map(i -> ThreadLocalRandom.current()
+                .nextInt(10, 500000 + 1)).forEachOrdered(randomNumbers::add);
+        readFile();
+    }
+
+    private static void readFile() {
+        Path file = null;
         try {
-            tokensList = Files.readAllLines(Paths.get("ru\\progwards\\java2\\lessons\\trees\\wiki.train.tokens"));
+            file = Paths.get
+                    ("C:\\Intellij Idea\\programming\\HelloWorld\\src\\ru\\progwards\\java2\\lessons\\trees\\wiki.train.tokens");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] words;
+        String line;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader
+                (new FileInputStream(String.valueOf(file)), StandardCharsets.UTF_8))){
+            while((line = br.readLine()) != null){
+                words = line.split("\\s");
+                for (String str : words) {
+                    if (str.matches("[a-zA-Zа-яёА-ЯЁ]+"))
+                        tokensList.add(str);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-
     public static void main(String[] args) {
+        fillingSortedData();
 
+//        randomNumbers.forEach(System.out::println);
+//        System.out.println(randomNumbers.size());
+//        sortedNumbers.forEach(System.out::println);
+//        System.out.println(sortedNumbers.size());
+
+
+        for (String s : tokensList) {
+            System.out.println(s);
+        }
+        System.out.println(tokensList.size());
     }
 }
