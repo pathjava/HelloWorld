fcb1c67d3b037344b143ab32f4e0fc513f689ab3
Oleg K <severniyman@gmail.com>
2020-06-15 11:27:18 +0300

    trees

--- src/ru/progwards/java2/lessons/trees/AVLTreeAndTreeMapTest.java
+++ src/ru/progwards/java2/lessons/trees/AVLTreeAndTreeMapTest.java
@@ -330,6 +330,7 @@ public class AVLTreeAndTreeMapTest {
         else if (operation.contains("findSort"))
             str = "Время поиска по сортированным ключам в";
 
+        System.out.println("Количество тестовых циклов: " + COUNT_LOOP + "\n");
         System.out.printf("%-39s %-12s %-12s %n", str, "AVL Tree", "TreeMap");
         System.out.printf("%-41s %-12d %d %n", "Минимальное время теста: ", resAvl.get(0), resMap.get(0));
         System.out.printf("%-41s %-12.1f %.1f %n", "Среднее арифметическое время теста: ",
