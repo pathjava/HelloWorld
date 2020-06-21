faf6985e608f0af614ac62f9d9d7d1748b838953
Oleg K <severniyman@gmail.com>
2020-06-21 19:39:49 +0300

    test10

---
+++ src/ru/progwards/sever/testprogwards2/test_10/CheckSum.java
@@ -0,0 +1,29 @@
+// Oleg Kiselev
+// 21.06.2020, 12:31
+
+package ru.progwards.sever.testprogwards2.test_10;
+
+public class CheckSum implements Task {
+    private long modifiedTime;
+
+    @Override
+    public long getModifiedTime() {
+        return modifiedTime;
+    }
+
+    @Override
+    public void setModifiedTime(long time) {
+        modifiedTime = time;
+    }
+
+    @Override
+    public String process(byte[] data) {
+        if (data.length == 0)
+            return "Нет данных";
+        byte checkSum = 0;
+        for (int i = 0; i < data.length; i++) {
+            checkSum += data[i];
+        }
+        return "Контрольная сумма: " + checkSum;
+    }
+}
---
+++ src/ru/progwards/sever/testprogwards2/test_10/MathExpectation.java
@@ -0,0 +1,29 @@
+// Oleg Kiselev
+// 21.06.2020, 12:31
+
+package ru.progwards.sever.testprogwards2.test_10;
+
+public class MathExpectation implements Task {
+    private long modifiedTime;
+
+    @Override
+    public long getModifiedTime() {
+        return modifiedTime;
+    }
+
+    @Override
+    public void setModifiedTime(long time) {
+        modifiedTime = time;
+    }
+
+    @Override
+    public String process(byte[] data) {
+        if (data.length == 0)
+            return "Нет данных";
+        long sum = 0;
+        for (int i = 0; i < data.length; i++)
+            sum += data[i];
+        double mathExpectation = (double) sum / data.length;
+        return "Мат ожидание: " + mathExpectation;
+    }
+}
