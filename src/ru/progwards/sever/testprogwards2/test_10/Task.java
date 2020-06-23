444a8cd2f7ee37cbf98f199fff37646e6e455c44
Oleg K <severniyman@gmail.com>
2020-06-23 19:30:08 +0300

    test10

--- src/ru/progwards/sever/testprogwards2/test_10/Task.java
+++ src/ru/progwards/sever/testprogwards2/test_10/Task.java
@@ -4,13 +4,11 @@
 package ru.progwards.sever.testprogwards2.test_10;
 
 public interface Task {
-    // методы для получения и установки
-    // времени создания файла
+    // методы для получения и установки времени создания файла
     long getModifiedTime();
 
     void setModifiedTime(long time);
 
-    // метод для обработки данных
-    // и возвращения результата в виде строки
+    // метод для обработки данных и возвращения результата в виде строки
     String process(byte[] data);
 }
