d774806fa11c7b3b33ffda5af0d2bfddba278d99
Oleg K <severniyman@gmail.com>
2020-06-26 18:09:43 +0300

    classloader

--- src/ru/progwards/java2/lessons/classloader/PathLoader.java
+++ src/ru/progwards/java2/lessons/classloader/PathLoader.java
@@ -16,9 +16,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PathLoader extends ClassLoader {
-    private final static String PATH_OF_TASKS = "C:/Intellij Idea/programming/HelloWorld/src/ru/progwards/java2/lessons/classloader/root/";
     private final static String DOT_CLASS = ".class";
-    private static PathLoader loader = new PathLoader(PATH_OF_TASKS);
+    private static PathLoader loader;
     private final String basePath;
 
     public PathLoader(String basePath) {
@@ -55,9 +54,10 @@ public class PathLoader extends ClassLoader {
         return c;
     }
 
-    private static void updateTaskList(Map<String, Task> tasks)
+    private void updateTaskList(Map<String, Task> tasks, PathLoader pathLoader)
             throws IOException {
-        Files.walkFileTree(Paths.get(PATH_OF_TASKS), new SimpleFileVisitor<>() {
+        loader = pathLoader;
+        Files.walkFileTree(Paths.get(basePath), new SimpleFileVisitor<>() {
             @Override
             public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
                 if (path.toString().endsWith(DOT_CLASS)) {
@@ -67,7 +67,7 @@ public class PathLoader extends ClassLoader {
                     if (task == null || task.getModifiedTime() < attrs.lastModifiedTime().toMillis()) {
                         try {
                             if (task != null)
-                                loader = new PathLoader(PATH_OF_TASKS);
+                                loader = new PathLoader(basePath);
                             Class<?> taskClass = loader.loadClass(className, true);
                             Task newTask = (Task) taskClass.getDeclaredConstructor().newInstance();
                             newTask.setModifiedTime(attrs.lastModifiedTime().toMillis());
@@ -91,24 +91,24 @@ public class PathLoader extends ClassLoader {
         });
     }
 
-    private static String makeClassName(Path path) throws IOException {
+    private String makeClassName(Path path) throws IOException {
         path = path.toAbsolutePath().toRealPath();
-        Path relPath = Paths.get(PATH_OF_TASKS).relativize(path);
+        Path relPath = Paths.get(basePath).relativize(path);
         String className = relPath.toString().replaceAll("[\\/\\\\]", ".");
         if (className.toLowerCase().endsWith(DOT_CLASS))
             className = className.substring(0, className.length() - DOT_CLASS.length());
         return className;
     }
 
-    private static void patchLoadedSuccessfully(String className) {
+    private void patchLoadedSuccessfully(String className) {
         try (FileWriter logFile = new FileWriter(getPathLogFile(), true)) {
-            logFile.write(getDateTimeLoadFile() + " " + className + " загружен из " + PATH_OF_TASKS + " успешно\n");
+            logFile.write(getDateTimeLoadFile() + " " + className + " загружен из " + basePath + " успешно\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void patchNotLoaded(String className, Exception eOne) {
+    private void patchNotLoaded(String className, Exception eOne) {
         try (FileWriter logFile = new FileWriter(getPathLogFile(), true)) {
             logFile.write(getDateTimeLoadFile() + " " + className + " ошибка загрузки " + eOne + " \n");
         } catch (IOException e) {
@@ -116,13 +116,13 @@ public class PathLoader extends ClassLoader {
         }
     }
 
-    private static String getDateTimeLoadFile() {
+    private String getDateTimeLoadFile() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         return now.format(formatter);
     }
 
-    private static String getPathLogFile() {
+    private String getPathLogFile() {
         String directory = System.getProperty("user.dir");
         String namePackage = PathLoader.class.getName();
         int index = namePackage.lastIndexOf(".");
@@ -133,11 +133,12 @@ public class PathLoader extends ClassLoader {
     }
 
     public static void main(String[] args) throws Exception {
+        PathLoader pathLoader = new PathLoader("C:/Intellij Idea/programming/HelloWorld/src/ru/progwards/java2/lessons/classloader/root/");
         Map<String, Task> tasks = new LinkedHashMap<>();
         while (true) {
             System.out.println("Проверка классов и запуск задач: " +
                     String.format("%1$tI:%1$tM:%1$tS.%1$tN", new Date()));
-            updateTaskList(tasks);
+            pathLoader.updateTaskList(tasks, pathLoader);
             SecureRandom random = new SecureRandom();
             byte[] data = new byte[1000];
             random.nextBytes(data);
