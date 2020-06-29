4302ea6a3adc4b479560c231907da94fe4cda940
Oleg K <severniyman@gmail.com>
2020-06-29 14:44:35 +0300

    classloader

--- src/ru/progwards/java2/lessons/classloader/profiler/ProfilerTransformer.java
+++ src/ru/progwards/java2/lessons/classloader/profiler/ProfilerTransformer.java
@@ -8,6 +8,8 @@ import javassist.*;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfilerTransformer implements ClassFileTransformer {
     @Override
@@ -20,12 +22,13 @@ public class ProfilerTransformer implements ClassFileTransformer {
             try {
                 ClassPool cp = ClassPool.getDefault();
                 cp.importPackage("ru.progwards.java1.lessons.datetime");
+                cp.importPackage("ru.progwards.java2.lessons.classloader.profiler");
                 System.out.println("name class " + className); /* for testing */
                 CtClass ct = cp.get(className.replace("/", "."));
 
                 CtMethod[] ctMethods = ct.getDeclaredMethods();
                 for (CtMethod ctMethod : ctMethods) {
-                    if (!ctMethod.getName().contains("fillArray")) {
+                    if (!excludedMethods(ctMethod)) {
                         System.out.println("method name start " + ctMethod.getName()); /* for testing */
 
                         String nameEnterSection = "Profiler.enterSection(\"" + ctMethod.getLongName() + "\");";
@@ -34,6 +37,9 @@ public class ProfilerTransformer implements ClassFileTransformer {
                         ctMethod.insertAfter(nameExitSection);
 
                         System.out.println("method name end " + ctMethod.getName()); /* for testing */
+                    } else if (ctMethod.getName().contains("main")) {
+//                        ctMethod.insertAfter("System.out.println(\"print something\");");
+                        ctMethod.insertAfter("ProfilerTransformer.printStatisticInfo();");
                     }
                 }
                 classfileBuffer = ct.toBytecode();
@@ -43,5 +49,17 @@ public class ProfilerTransformer implements ClassFileTransformer {
             }
         }
         return classfileBuffer;
+    }
+
+    private boolean excludedMethods(CtMethod ctMethod) {
+        List<String> methods = new ArrayList<>(List.of("fillArray", "main"));
+        for (String method : methods)
+            if (method.equals(ctMethod.getName()))
+                return true;
+        return false;
+    }
+
+    public static void printStatisticInfo() {
+        System.out.println("print something");
     }
 }
--- src/ru/progwards/java2/lessons/classloader/profiler/TestSpeed.java
+++ src/ru/progwards/java2/lessons/classloader/profiler/TestSpeed.java
@@ -3,8 +3,11 @@
 
 package ru.progwards.java2.lessons.classloader.profiler;
 
+import ru.progwards.java1.lessons.datetime.Profiler;
+
 import java.security.SecureRandom;
 import java.util.Arrays;
+
 
 public class TestSpeed {
 
@@ -22,6 +25,7 @@ public class TestSpeed {
     }
 
     public void bubbleSort() {
+        Profiler.enterSection("ru.progwards.java2.lessons.classloader.profiler.TestSpeed.bubbleSort()");
         int[] arr = Arrays.copyOf(tempArray, tempArray.length);
         for (int i = 0; i < arr.length - 1; i++)
             for (int j = 0; j < arr.length - i - 1; j++)
@@ -32,6 +36,7 @@ public class TestSpeed {
                 }
         if (showResultSort)
             Arrays.stream(arr).forEach(System.out::println);
+        Profiler.exitSection("ru.progwards.java2.lessons.classloader.profiler.TestSpeed.bubbleSort()");
     }
 
     public void selectionSort() {
@@ -63,7 +68,7 @@ public class TestSpeed {
     }
 
     public static void main(String[] args) {
-        TestSpeed test = new TestSpeed(10000, false);
+        TestSpeed test = new TestSpeed(10, false);
         test.fillArray();
 
         test.bubbleSort();
