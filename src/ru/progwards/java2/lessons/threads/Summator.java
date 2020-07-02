f431dcb6d3a96836fd6913c508eb32960fb13a5d
Oleg K <severniyman@gmail.com>
2020-07-02 19:48:37 +0300

    threads

--- src/ru/progwards/java2/lessons/threads/Summator.java
+++ src/ru/progwards/java2/lessons/threads/Summator.java
@@ -7,7 +7,7 @@ import java.math.BigInteger;
 
 public class Summator {
 
-    private int count;
+    private final int count;
     private BigInteger counter;
 
     public Summator(int count) {
@@ -15,16 +15,50 @@ public class Summator {
         counter = BigInteger.ZERO;
     }
 
-    public BigInteger sum(BigInteger number){
-        for (BigInteger i = BigInteger.ZERO; i.compareTo(number) < 0; i = i.add(BigInteger.ONE)) {
-            counter = counter.add(BigInteger.ONE);
+    // 100 / 3 == 0~33, 34~66, 67~100
+    public BigInteger sum(BigInteger number) {
+        if (count > 1) {
+            BigInteger remainder = number.mod(BigInteger.valueOf(count));
+            for (int i = 0; i < count; i++) {
+                BigInteger partOfTheNumber = number.divide(BigInteger.valueOf(count));
+                BigInteger partLastOfTheNumber = null;
+                if (i == count - 1)
+                    partLastOfTheNumber = partOfTheNumber.add(remainder);
+                if (i == 0)
+                    creatorThreads(BigInteger.ONE, partOfTheNumber);
+                else if (i < count - 1)
+                    creatorThreads(partOfTheNumber.add(BigInteger.ONE), partOfTheNumber.add(partOfTheNumber));
+                else
+                    creatorThreads(partOfTheNumber.add(BigInteger.ONE), partOfTheNumber.add(partLastOfTheNumber));
+            }
         }
         return counter;
     }
 
+    private void creatorThreads(BigInteger startNumber, BigInteger stopNumber) {
+        Thread[] threads = new Thread[count];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (BigInteger i = startNumber; i.compareTo(stopNumber) <= 0; i = i.add(BigInteger.ONE))
+                        counter = counter.add(i);
+                }
+            });
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-        Summator summator = new Summator(10);
-        System.out.println(summator.sum(BigInteger.valueOf(10)));
+        Summator summator = new Summator(3);
+        System.out.println(summator.sum(BigInteger.valueOf(100)));
     }
 }
