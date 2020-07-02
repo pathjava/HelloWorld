cf8c379616b5316557c0edb382b91e77591e555c
Oleg K <severniyman@gmail.com>
2020-07-02 20:04:36 +0300

    threads

--- src/ru/progwards/java2/lessons/threads/Summator.java
+++ src/ru/progwards/java2/lessons/threads/Summator.java
@@ -17,19 +17,24 @@ public class Summator {
 
     // 100 / 3 == 0~33, 34~66, 67~100
     public BigInteger sum(BigInteger number) {
+        BigInteger tempStart = BigInteger.ONE;
+        BigInteger tempEnd = null;
         if (count > 1) {
             BigInteger remainder = number.mod(BigInteger.valueOf(count));
             for (int i = 0; i < count; i++) {
                 BigInteger partOfTheNumber = number.divide(BigInteger.valueOf(count));
-                BigInteger partLastOfTheNumber = null;
-                if (i == count - 1)
-                    partLastOfTheNumber = partOfTheNumber.add(remainder);
-                if (i == 0)
+                if (i == 0) {
+                    tempEnd = partOfTheNumber;
                     creatorThreads(BigInteger.ONE, partOfTheNumber);
-                else if (i < count - 1)
+                } else if (i < count - 1) {
+                    tempStart = tempEnd.add(BigInteger.ONE);
+                    tempEnd = tempStart.add(partOfTheNumber.subtract(BigInteger.ONE));
                     creatorThreads(partOfTheNumber.add(BigInteger.ONE), partOfTheNumber.add(partOfTheNumber));
-                else
-                    creatorThreads(partOfTheNumber.add(BigInteger.ONE), partOfTheNumber.add(partLastOfTheNumber));
+                } else {
+                    tempStart = tempEnd.add(BigInteger.ONE);
+                    tempEnd = tempStart.add(partOfTheNumber);
+                    creatorThreads(partOfTheNumber.add(BigInteger.ONE), partOfTheNumber.add(partOfTheNumber));
+                }
             }
         }
         return counter;
