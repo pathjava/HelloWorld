5308918b961a730c9c3480b4d245da7c1c2da232
Oleg K <severniyman@gmail.com>
2020-07-02 15:36:34 +0300

    threads

--- src/ru/progwards/java2/lessons/threads/Summator.java
+++ src/ru/progwards/java2/lessons/threads/Summator.java
@@ -3,5 +3,25 @@
 
 package ru.progwards.java2.lessons.threads;
 
+import java.math.BigInteger;
+
 public class Summator {
+
+    private int count;
+    private BigInteger counter;
+
+    public Summator(int count) {
+        this.count = count;
+        counter = BigInteger.ONE;
+    }
+
+    public BigInteger sum(BigInteger number){
+
+        return counter;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
