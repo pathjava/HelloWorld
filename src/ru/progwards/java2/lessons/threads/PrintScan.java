69b35e1320c08c42624f0a50ae71215daeb2f21a
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-04 12:39:59 +0300

    threads

--- src/ru/progwards/java2/lessons/threads/PrintScan.java
+++ src/ru/progwards/java2/lessons/threads/PrintScan.java
@@ -13,6 +13,7 @@ import java.util.stream.IntStream;
 public class PrintScan {
 
     //-------------Synchronized
+
     private static final Object lockPrint = new Object();
     private static final Object lockScan = new Object();
 
