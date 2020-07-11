09f30ede10eb6476dcb84336ab700314b766d11e
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-11 19:12:58 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/Dijkstra.java
+++ src/ru/progwards/java2/lessons/graph/Dijkstra.java
@@ -3,9 +3,12 @@
 
 package ru.progwards.java2.lessons.graph;
 
+import java.util.Map;
+
 public class Dijkstra {
 
     private int[][] graph;
+    private Map<>
 
     public Dijkstra(int[][] graph) {
         this.graph = graph;
@@ -66,6 +69,16 @@ public class Dijkstra {
                 {0, 0, 0, 0, 0, 0, 0, 0, 15},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-        Dijkstra dijkstra = new Dijkstra(matrix);
+        int[][] oriMatrix = {{0, 10, 6, 8, 0, 0, 0, 0, 0},
+                {10, 0, 0, 5, 13, 0, 11, 0, 0},
+                {6, 0, 0, 2, 3, 0, 0, 0, 0},
+                {8, 5, 2, 0, 5, 7, 12, 0, 0},
+                {0, 13, 3, 5, 0, 9, 0, 0, 12},
+                {0, 0, 7, 0, 9, 0, 4, 8, 10},
+                {0, 11, 0, 12, 0, 4, 0, 6, 16},
+                {0, 0, 0, 0, 0, 8, 6, 0, 15},
+                {0, 0, 0, 0, 12, 10, 16, 15, 0}};
+
+        Dijkstra dijkstra = new Dijkstra(oriMatrix);
     }
 }
