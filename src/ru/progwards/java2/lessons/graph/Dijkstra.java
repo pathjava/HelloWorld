c419fd48d48088621b5218e00df5b33082e61772
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-11 11:47:39 +0300

    graph

---
+++ src/ru/progwards/java2/lessons/graph/Dijkstra.java
@@ -0,0 +1,22 @@
+// Oleg Kiselev
+// 11.07.2020, 10:07
+
+package ru.progwards.java2.lessons.graph;
+
+public class Dijkstra {
+
+    private int[][] graph;
+
+    public Dijkstra(int[][] graph) {
+        this.graph = graph;
+    }
+
+    public int[][] find(int n) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
