d54973e99d6d65e6fe5d2b2427f9c23aab3f5dc3
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-13 20:51:22 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/Dijkstra.java
+++ src/ru/progwards/java2/lessons/graph/Dijkstra.java
@@ -22,50 +22,49 @@ public class Dijkstra {
         while (count < graph.length) {
             int key = queue.isEmpty() ? n : queue.poll(); /* ключ обрабатываемого узла */
             if (!nodes.get(key).visited) { /* если узел не отмечен как посещенный */
-                searchPathsToNodes(nodes.get(key), key); /* ищем и добавляем в TreeSet в узле информацию о ближайших узлах */
+                searchPathsToNodes(nodes.get(key), key); /* ищем и добавляем в TreeSet в узле информацию о ближайших (смежных) узлах */
                 nodes.get(key).visited = true; /* отмечаем узел как посещенный */
             }
 
-            for (NodeSet sortedNode : nodes.get(key).sortedNodes) {
-                int path = nodes.get(key).pathLength + sortedNode.pathLengthSet;
-                if (nodes.containsKey(sortedNode.numberNodeSet)) {
-                    if (nodes.get(key).pathLength > path) {
-                        nodes.get(key).pathLength = path;
-                        nodes.get(key).comeFrom = sortedNode.comeFromSet;
+            for (NodeSet sortedNode : nodes.get(key).sortedNodes) { /* в цикле обрабатываем ранее собранную информацию о ближайших (смежных) узлах */
+                int pathSize = nodes.get(key).pathLength + sortedNode.pathLengthSet; /* длина пути от стартового N узла до текущего + до смежного */
+                if (nodes.containsKey(sortedNode.numberNodeSet)) { /* если такой узел уже есть в nodes */
+                    if (nodes.get(key).pathLength > pathSize) { /* проверяем, является ли новый путь короче старого */
+                        nodes.get(key).pathLength = pathSize; /* если да, то обновляем путь */
+                        nodes.get(key).comeFrom = sortedNode.comeFromSet; /* обновляем информацию о узле из которого пришел новый путь */
                     }
-                } else {
+                } else { /* если узла в nodes нет, то создаем его */
                     node = new Node();
                     node.numberNode = sortedNode.numberNodeSet;
                     node.comeFrom = sortedNode.comeFromSet;
-                    node.pathLength = path;
+                    node.pathLength = pathSize;
                     node.visited = sortedNode.visitedSet;
                     nodes.put(sortedNode.numberNodeSet, node);
                 }
             }
             count++;
         }
-        for (Map.Entry<Integer, Node> entry : nodes.entrySet()) {
-            System.out.println(entry);
-        }
+        /* вывод информации о кратчайших путях до каждого узла от узла N */
+        nodes.entrySet().forEach(System.out::println);
     }
 
-    private void initializationFirstNode(int n) {
+    private void initializationFirstNode(int n) { /* инициализация N узла */
         node = new Node();
         node.numberNode = n;
         node.pathLength = 0;
         nodes.put(n, node);
     }
 
-    private void searchPathsToNodes(Node node, int key) {
+    private void searchPathsToNodes(Node node, int key) { /* формирование смежных узлов в sortedNodes внутри узла */
         for (int i = 0; i < graph.length; i++) {
-            if (graph[key][i] != 0 && i != node.comeFrom) {
-                if (nodes.containsKey(i)) {
-                    int pathSize = graph[key][i] + node.pathLength;
-                    if (pathSize < nodes.get(i).pathLength) {
-                        nodes.get(i).pathLength = pathSize;
-                        nodes.get(i).comeFrom = key;
+            if (graph[key][i] != 0 && i != node.comeFrom) { /* если ячейка не 0 и не является узлом-родителем */
+                if (nodes.containsKey(i)) { /* если смежный узел уже есть в nodes */
+                    int pathSize = graph[key][i] + node.pathLength; /* длина пути от стартового N узла до текущего + до смежного */
+                    if (pathSize < nodes.get(i).pathLength) { /* проверяем, является ли новый путь короче старого */
+                        nodes.get(i).pathLength = pathSize; /* если да, то обновляем путь */
+                        nodes.get(i).comeFrom = key; /* обновляем информацию о узле из которого пришел новый путь */
                     }
-                } else {
+                } else { /* если узла в sortedNodes нет, то создаем его */
                     NodeSet nodeSet = new NodeSet();
                     nodeSet.pathLengthSet = graph[key][i];
                     nodeSet.numberNodeSet = i;
@@ -74,11 +73,11 @@ public class Dijkstra {
                 }
             }
         }
-        for (NodeSet sortedNode : node.sortedNodes)
+        for (NodeSet sortedNode : node.sortedNodes) /* формируем очередь приоритета обработки узлов */
             queue.add(sortedNode.numberNodeSet);
     }
 
-    static class Node {
+    static class Node { /* основной узел из nodes */
         private boolean visited = false;
         private int comeFrom = 0;
         private int numberNode = 0;
@@ -100,7 +99,7 @@ public class Dijkstra {
         }
     }
 
-    static class NodeSet {
+    static class NodeSet { /* вспомогательный узел для хранения смежных узлов в sortedNodes */
         private final boolean visitedSet = false;
         private int comeFromSet = 0;
         private int numberNodeSet = 0;
