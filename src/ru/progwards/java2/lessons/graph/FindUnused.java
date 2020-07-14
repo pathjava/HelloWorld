53c836fd3648718858434d16c180f77ee8afdc09
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-14 11:52:17 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/FindUnused.java
+++ src/ru/progwards/java2/lessons/graph/FindUnused.java
@@ -3,5 +3,26 @@
 
 package ru.progwards.java2.lessons.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindUnused {
+
+    public static List<CObject> find(List<CObject> roots, List<CObject> objects) {
+
+
+    }
+
+    public static class CObject {
+        private List<CObject> references;
+        private int mark;
+    }
+
+
+    public static void main(String[] args) {
+        List<CObject> roots = new ArrayList<>();
+        List<CObject> objects = new ArrayList<>();
+
+        find(roots, objects);
+    }
 }
