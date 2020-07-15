77b882b28db7cdb6a623ae245cf35b7de434d616
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-15 19:52:35 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/FindUnused.java
+++ src/ru/progwards/java2/lessons/graph/FindUnused.java
@@ -5,6 +5,7 @@ package ru.progwards.java2.lessons.graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FindUnused {
 
@@ -19,17 +20,50 @@ public class FindUnused {
 
     public static class CObject {
         private List<CObject> references;
+        private String nameNode;
         private final State mark = State.UNUSED;
         // UNUSED - не используется
         // CURRENT - используется
         // USED - посещен
     }
 
+    public static List<CObject> graphCreator() {
+        List<CObject> objects = new ArrayList<>();
+        CObject cObject;
+        int rand = ThreadLocalRandom.current().nextInt(15, 30);
+
+        for (int i = 0; i < rand; i++) {
+            cObject = new CObject();
+            cObject.nameNode = "NodeGraph-" + i;
+            objects.add(cObject);
+        }
+        return objects;
+    }
+
+    public static List<CObject> rootCreator(int n) {
+        List<CObject> roots = new ArrayList<>();
+        CObject cObject;
+
+        for (int i = 0; i < n; i++) {
+            cObject = new CObject();
+            cObject.nameNode = "NodeRoot-" + i;
+            cObject.references = graphCreator();
+            roots.add(cObject);
+        }
+        return roots;
+    }
+
 
     public static void main(String[] args) {
-        List<CObject> roots = new ArrayList<>();
-        List<CObject> objects = new ArrayList<>();
+        int rootCount = 5;
+        List<CObject> roots;
+        List<CObject> objects;
 
-        find(roots, objects);
+        while (rootCount > 0) {
+            roots = rootCreator();
+            objects = graphCreator();
+            find(roots, objects);
+            rootCount--;
+        }
     }
 }
