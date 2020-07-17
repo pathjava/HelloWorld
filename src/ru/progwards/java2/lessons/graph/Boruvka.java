031a0e2a822bfc710634f2db1764b37829d4a39d
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-17 17:39:19 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/Boruvka.java
+++ src/ru/progwards/java2/lessons/graph/Boruvka.java
@@ -3,8 +3,7 @@
 
 package ru.progwards.java2.lessons.graph;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Boruvka<N, E> {
 
@@ -22,6 +21,43 @@ public class Boruvka<N, E> {
 
     }
 
+    static class Node<N, E> {
+        N info; // информация об узле
+        List<Edge<N, E>> in; // массив входящих ребер
+        List<Edge<N, E>> out; // массив исходящих ребер
+        String name; // имя узла
+
+        public Node() {
+            in = new ArrayList<>();
+            out = new ArrayList<>();
+            in.sort((o1, o2) -> {
+                if (o1.weight > o2.weight)
+                    return 1;
+                else if (o1.weight < o2.weight)
+                    return -1;
+                else {
+                    if (o1.id > o2.id)
+                        return 1;
+                    else if (o1.id < o2.id)
+                        return -1;
+                }
+                return 0;
+            });
+        }
+    }
+
+    static class Edge<N, E> {
+        E info; // информация о ребре
+        Node<N, E> out; // вершина, из которой исходит ребро
+        Node<N, E> in; // вершина, в которую можно попасть по этому ребру
+        double weight; // стоимость перехода
+        int id; // уникальный идентификатор ребра
+    }
+
+    static class Graph<N, E> {
+        List<Node<N, E>> nodes;
+        List<Edge<N, E>> edges;
+    }
 
     public static void main(String[] args) {
         Boruvka<String, Integer> boruvka = new Boruvka<>();
@@ -29,24 +65,4 @@ public class Boruvka<N, E> {
         Node<String, Integer> node = new Node<>();
         Edge<String, Integer> edge = new Edge<>();
     }
-}
-
-class Node<N, E> {
-    N info; // информация об узле
-    List<Edge<N, E>> in; // массив входящих ребер
-    List<Edge<N, E>> out; // массив исходящих ребер
-    String name; // имя узла
-}
-
-class Edge<N, E> {
-    E info; // информация о ребре
-    Node<N, E> out; // вершина, из которой исходит ребро
-    Node<N, E> in; // вершина, в которую можно попасть по этому ребру
-    double weight; // стоимость перехода
-    int id; // уникальный идентификатор ребра
-}
-
-class Graph<N, E> {
-    List<Node<N, E>> nodes;
-    List<Edge<N, E>> edges;
 }
