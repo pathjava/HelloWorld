e9c643e50bf39fb8251271fbb35ab2e234acdb1a
severniyman <A~=107&iT@W0xS4my@nV>
2020-07-17 15:56:13 +0300

    graph

--- src/ru/progwards/java2/lessons/graph/Boruvka.java
+++ src/ru/progwards/java2/lessons/graph/Boruvka.java
@@ -14,26 +14,39 @@ public class Boruvka<N, E> {
         return edgeList;
     }
 
-    private class Node<N, E> {
-        N info; // информация об узле
-        List<Edge<N, E>> in; // массив входящих ребер
-        List<Edge<N, E>> out; // массив исходящих ребер
+    private void find() {
+
     }
 
-    private class Edge<N, E> {
-        E info; // информация о ребре
-        Node<N, E> out; // вершина, из которой исходит ребро
-        Node<N, E> in; // вершина, в которую можно попасть по этому ребру
-        double weight; // стоимость перехода
-    }
+    private void merge() {
 
-    private class Graph<N, E> {
-        List<Node<N, E>> nodes;
-        List<Edge<N, E>> edges;
     }
 
 
     public static void main(String[] args) {
-
+        Boruvka<String, Integer> boruvka = new Boruvka<>();
+        Graph<String, Integer> graph = new Graph<>();
+        Node<String, Integer> node = new Node<>();
+        Edge<String, Integer> edge = new Edge<>();
     }
+}
+
+class Node<N, E> {
+    N info; // информация об узле
+    List<Edge<N, E>> in; // массив входящих ребер
+    List<Edge<N, E>> out; // массив исходящих ребер
+    String name; // имя узла
+}
+
+class Edge<N, E> {
+    E info; // информация о ребре
+    Node<N, E> out; // вершина, из которой исходит ребро
+    Node<N, E> in; // вершина, в которую можно попасть по этому ребру
+    double weight; // стоимость перехода
+    int id; // уникальный идентификатор ребра
+}
+
+class Graph<N, E> {
+    List<Node<N, E>> nodes;
+    List<Edge<N, E>> edges;
 }
