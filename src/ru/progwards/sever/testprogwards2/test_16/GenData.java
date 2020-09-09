98db8bf898f1322c0c397f59e865d9b39896d0f3
Oleg K <severniyman@gmail.com>
2020-09-09 15:55:09 +0300

    gc

---
+++ src/ru/progwards/sever/testprogwards2/test_16/GenData.java
@@ -0,0 +1,29 @@
+package ru.progwards.sever.testprogwards2.test_16;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GenData {
+    static void generate() {
+        PrintWriter file = null;
+        try {
+            file = new PrintWriter(new FileOutputStream(new File("data.txt")));
+            for(int i=0; i<200_000_000; i++)
+                file.println(ThreadLocalRandom.current().nextInt());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (file != null)
+                file.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        generate();
+        System.out.println("stop");
+    }
+}
